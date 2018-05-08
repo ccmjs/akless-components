3f835b1a3a0fa8f967b04ded1d6585526d333c98
akless <andre.kless@web.de>
2018-05-08 21:14:29 +0200

    use existing user component version for example teambuild configurations

--- teambuild/resources/configs.js
+++ teambuild/resources/configs.js
@@ -20,7 +20,7 @@ ccm.files[ 'configs.js' ] = {
       "store": [ "ccm.store", { "store": "teambuild", "url": "wss://ccm2.inf.h-brs.de", "db": "redis" } ],
       "key": "demo"
     },
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "guest" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "guest" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "greedy" ] ]
   },
   "clicker": {
@@ -32,7 +32,7 @@ ccm.files[ 'configs.js' ] = {
     "names": [ "Stimmt", "Stimmt nicht", "Kommt drauf an" ],
     "max_teams": 3,
     "editable": { "join": true, "leave": true, "rename": false },
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "demo" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "demo" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "greedy" ] ]
   },
   "se_ws17_gr1": {  // created for ccm.teambuild-1.0.0.js
@@ -43,7 +43,7 @@ ccm.files[ 'configs.js' ] = {
     },
     "editable": false,
     "max_members": 3,
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "se_ws17_teambuild" ] ]
   },
   "se_ws17_gr2": {  // created for ccm.teambuild-1.0.0.js
@@ -54,7 +54,7 @@ ccm.files[ 'configs.js' ] = {
     },
     "editable": false,
     "max_members": 3,
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "se_ws17_teambuild" ] ]
   },
   "se_ws17_gr3": {  // created for ccm.teambuild-1.0.0.js
@@ -65,7 +65,7 @@ ccm.files[ 'configs.js' ] = {
     },
     "editable": false,
     "max_members": 3,
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "se_ws17_teambuild" ] ]
   },
   "se_ws17_gr4": {  // created for ccm.teambuild-1.0.0.js
@@ -76,7 +76,7 @@ ccm.files[ 'configs.js' ] = {
     },
     "editable": false,
     "max_members": 3,
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "se_ws17_teambuild" ] ]
   },
   "se_ws17_gr5": {  // created for ccm.teambuild-1.0.0.js
@@ -87,7 +87,7 @@ ccm.files[ 'configs.js' ] = {
     },
     "editable": false,
     "max_members": 3,
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "se_ws17_teambuild" ] ]
   },
   "se_ws17_gr6": {  // created for ccm.teambuild-1.0.0.js
@@ -98,7 +98,7 @@ ccm.files[ 'configs.js' ] = {
     },
     "editable": false,
     "max_members": 3,
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "se_ws17_teambuild" ] ]
   },
   "sks_ws17": {  // created for ccm.teambuild-1.0.0.js
@@ -111,7 +111,7 @@ ccm.files[ 'configs.js' ] = {
     "max_teams": 4,
     "max_members": 12,
     "editable": false,
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.0.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-2.0.1.min.js", { "logged_in": true, "sign_on": "hbrsinfkaul" } ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-1.0.0.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "sks_ws17_teambuild" ] ]
   }
 };
