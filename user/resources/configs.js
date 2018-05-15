2bc670504ed28e09ecb05efb72bfdb6f33d9f505
akless <andre.kless@web.de>
2018-05-15 23:13:28 +0200

    ccm.user.js v5.0.0: updated demo authentication

--- user/resources/configs.js
+++ user/resources/configs.js
@@ -12,8 +12,11 @@ ccm.files[ 'configs.js' ] = {
   },
   "demo": {
     "realm": "demo",
-    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/user/resources/default.css" ],
-    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "greedy" ] ]
+    "css": [ "ccm.load",
+      "https://ccmjs.github.io/akless-components/user/resources/tea.css",
+      "https://ccmjs.github.io/tkless-components/libs/bootstrap/css/bootstrap.css",
+      { "context": "head", "url": "https://ccmjs.github.io/tkless-components/libs/bootstrap/css/font-face.css" }
+    ]
   },
   "lea": {
     "realm": "LEA",
