5c6f8ad8e8578d3c67045d9f9fce512f0dd1c7ae
akless <andre.kless@web.de>
2018-05-22 23:45:29 +0200

    remove not needed property

--- user/resources/configs.js
+++ user/resources/configs.js
@@ -6,7 +6,6 @@
 
 ccm.files[ 'configs.js' ] = {
   "local": {
-    "realm": "guest",
     "css": [ "ccm.load", "../user/resources/default.css" ],
     "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ]
   },
