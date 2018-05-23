d43db5a41b1ae27154c23769d01e478f162d8cde
akless <andre.kless@web.de>
2018-05-23 23:27:36 +0200

    minor change

--- cloze_builder/resources/configs.js
+++ cloze_builder/resources/configs.js
@@ -1,5 +1,5 @@
 /**
- * @overview configurations of ccm component for rendering a fill-in-the-blank text
+ * @overview configurations of ccm component for building a fill-in-the-blank text
  * @author André Kless <andre.kless@web.de> 2017-2018
  * @license The MIT License (MIT)
  */
