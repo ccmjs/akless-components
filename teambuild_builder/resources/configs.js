0824eb5420b9dfa343ef694d78cd5b24fd5303d6
akless <andre.kless@web.de>
2018-05-29 22:12:51 +0200

    add CRUD configuration at teambuild

--- teambuild_builder/resources/configs.js
+++ teambuild_builder/resources/configs.js
@@ -91,6 +91,41 @@ ccm.files[ 'configs.js' ] = {
       },
       "alert": "Saved!"
     }
+  },
+
+  "crud": {
+    "css": [ "ccm.load",
+      "https://ccmjs.github.io/tkless-components/libs/bootstrap/css/bootstrap.css",
+      { "context": "head", "url": "https://ccmjs.github.io/tkless-components/libs/bootstrap/css/font-face.css" },
+      "https://ccmjs.github.io/akless-components/teambuild_builder/resources/default.css"
+    ],
+    "data": {
+      "store": [ "ccm.store", { "store": "teambuild", "url": "https://ccm2.inf.h-brs.de" } ]
+    },
+    "defaults": {
+      "editable": {
+        "join": true,
+        "leave": true,
+        "rename": true
+      },
+      "text": {
+        "team": "Team",
+        "leave": "leave",
+        "join": "join",
+        "free": "free",
+        "message": "Nothing to display."
+      },
+      "data.store": "['ccm.store',{'store':'teambuild_data','url':'wss://ccm2.inf.h-brs.de'}]",
+      "user": "['ccm.instance','https://ccmjs.github.io/akless-components/user/versions/ccm.user-7.0.0.js',['ccm.get','https://ccmjs.github.io/akless-components/user/resources/configs.js','guest']]"
+    },
+    "submit_button": "Submit",
+    "target": [ "ccm.component", "https://ccmjs.github.io/akless-components/teambuild/versions/ccm.teambuild-2.0.0.js" ],
+    "onfinish": {
+      "store": {
+        "settings": { "store": "teambuild", "url": "https://ccm2.inf.h-brs.de" }
+      },
+      "alert": "Saved!"
+    }
   }
 
 };
