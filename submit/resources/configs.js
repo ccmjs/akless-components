03c6eaa04d18c78d5827077a715c725bc02d4090
akless <andre.kless@web.de>
2018-05-29 18:38:21 +0200

    updated configurations at submit/resources/configs.js

--- submit/resources/configs.js
+++ submit/resources/configs.js
@@ -12,8 +12,7 @@ ccm.files[ 'configs.js' ] = {
       "key": "test"
     },
     "content": [ "ccm.component", "../content/ccm.content.js" ],
-    "cloze_builder": [ "ccm.component", "../cloze_builder/ccm.cloze_builder.js" ],
-    "user": [ "ccm.instance", "../user/ccm.user.js" ],
+    "cloze_builder": [ "ccm.component", "../cloze_builder/ccm.cloze_builder.js", { "submit_button": false } ],
     "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
     "onfinish": { "log": true }
   },
@@ -24,8 +23,7 @@ ccm.files[ 'configs.js' ] = {
       "key": "test"
     },
     "content": [ "ccm.component", "../content/ccm.content.js" ],
-    "cloze_builder": [ "ccm.component", "../cloze_builder/ccm.cloze_builder.js" ],
-    "user": [ "ccm.instance", "../user/ccm.user.js" ],
+    "cloze_builder": [ "ccm.component", "../cloze_builder/ccm.cloze_builder.js", { "submit_button": false } ],
     "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
     "onfinish": {
       "log": true,
@@ -43,11 +41,8 @@ ccm.files[ 'configs.js' ] = {
       "key": "demo"
     },
     "content": [ "ccm.component", "https://ccmjs.github.io/akless-components/content/versions/ccm.content-4.0.0.js" ],
-    "cloze_builder": [ "ccm.component", "https://ccmjs.github.io/akless-components/cloze_builder/versions/ccm.cloze_builder-2.1.3.js" ],
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-4.0.1.js", { "realm": "demo" } ],
-    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.1.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
+    "cloze_builder": [ "ccm.component", "https://ccmjs.github.io/akless-components/cloze_builder/versions/ccm.cloze_builder-2.2.0.js", { "submit_button": false } ],
     "onfinish": {
-      "log": true,
       "store": {
         "settings": { "store": "submit", "url": "https://ccm2.inf.h-brs.de" },
         "key": "demo"
