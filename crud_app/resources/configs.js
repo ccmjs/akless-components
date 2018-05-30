a3dcc371965299f8bfe15bd94448ab0a2f2dc489
akless <andre.kless@web.de>
2018-05-30 22:05:45 +0200

    updated configuration

--- crud_app/resources/configs.js
+++ crud_app/resources/configs.js
@@ -20,8 +20,50 @@ ccm.files[ 'configs.js' ] = {
     "store": [ "ccm.store", { "store": "cloze", "url": "https://ccm2.inf.h-brs.de" } ]
   },
   "teambuild": {
-    "builder": [ "ccm.component", "https://ccmjs.github.io/akless-components/teambuild_builder/versions/ccm.teambuild_builder-3.0.0.js" ],
+    "builder": [ "ccm.component", "https://ccmjs.github.io/akless-components/teambuild_builder/versions/ccm.teambuild_builder-3.0.0.js", { "submit_button": false } ],
     "store": [ "ccm.store", { "store": "teambuild", "url": "https://ccm2.inf.h-brs.de" } ],
     "url": "https://ccmjs.github.io/akless-components/teambuild/versions/ccm.teambuild-2.0.0.js"
+  },
+  "lea_cloze": {
+    "builder": [ "ccm.component", "https://ccmjs.github.io/akless-components/teambuild_builder/versions/ccm.teambuild_builder-3.0.0.js", {
+      "html.inner.1.inner.2.inner.0.inner.1.inner.1.inner.5": {
+        "tag": "h5",
+        "inner": "H-BRS LEA"
+      },
+      "html.inner.1.inner.2.inner.0.inner.1.inner.1.inner.6": {
+        "tag": "p",
+        "inner": "Authentication with a valid account from the LEA platform at Hochschule Bonn-Rhein-Sieg University of Applied Sciences."
+      },
+      "html.inner.1.inner.2.inner.0.inner.2.inner.3": {
+        "tag": "option",
+        "inner": "H-BRS LEA",
+        "value": "['ccm.instance','https://ccm-comp.bib.h-brs.de/ccm-components/user/versions/ccm.user-7.0.0.js',{'realm':'LEA'}]"
+      },
+      "defaults.user": "['ccm.instance','https://ccm-comp.bib.h-brs.de/ccm-components/user/versions/ccm.user-7.0.0.js',{'realm':'LEA'}]",
+      "defaults.css": "['ccm.load','https://ccm-comp.bib.h-brs.de/ccm-components/cloze/resources/lea.css','https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css',{'context':'head','url':'https://fonts.googleapis.com/css?family=Montserrat:200'}]",
+      "submit_button": false
+    } ],
+    "store": [ "ccm.store", { "store": "cloze", "url": "https://ccm2.inf.h-brs.de" } ]
+  },
+  "lea_teambuild": {
+    "builder": [ "ccm.component", "https://ccmjs.github.io/akless-components/teambuild_builder/versions/ccm.teambuild_builder-3.0.0.js", {
+      "html.inner.1.inner.2.inner.2.inner.1.inner.1.inner.5": {
+        "tag": "h5",
+        "inner": "H-BRS LEA"
+      },
+      "html.inner.1.inner.2.inner.2.inner.1.inner.1.inner.6": {
+        "tag": "p",
+        "inner": "Authentication with a valid account from the LEA platform at Hochschule Bonn-Rhein-Sieg University of Applied Sciences."
+      },
+      "html.inner.1.inner.2.inner.2.inner.2.inner.3": {
+        "tag": "option",
+        "inner": "H-BRS LEA",
+        "value": "['ccm.instance','https://ccm-comp.bib.h-brs.de/ccm-components/user/versions/ccm.user-7.0.0.js',{'realm':'LEA'}]"
+      },
+      "defaults.user": "['ccm.instance','https://ccm-comp.bib.h-brs.de/ccm-components/user/versions/ccm.user-7.0.0.js',{'realm':'LEA'}]",
+      "defaults.css": "['ccm.load','https://ccm-comp.bib.h-brs.de/ccm-components/cloze/resources/lea.css','https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css',{'context':'head','url':'https://fonts.googleapis.com/css?family=Montserrat:200'}]",
+      "submit_button": false
+    } ],
+    "store": [ "ccm.store", { "store": "teambuild", "url": "https://ccm2.inf.h-brs.de" } ]
   }
 };
