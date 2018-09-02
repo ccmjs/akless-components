27e10fb170a6d78aa8f6013283baedf848d57040
akless <andre.kless@web.de>
2018-09-02 13:38:55 +0200

    updated ccm.multi_blank.js -> async await

--- multi_blank/ccm.multi_blank.js
+++ multi_blank/ccm.multi_blank.js
@@ -1,34 +1,30 @@
 /**
- * @overview example component for reusing an undefined number of ccm instances of the same component
+ * @overview example ccm component for reusing any number of ccm instances of the same component
  * @author André Kless <andre.kless@web.de> 2017-2018
  * @license The MIT License (MIT)
  */
 
-{
-  var component = {
+( function () {
+
+  const component = {
 
     name: 'multi_blank',
 
     ccm: 'https://ccmjs.github.io/ccm/ccm.js',
 
     config:  {
-      component_obj: [ "ccm.component", "https://ccmjs.github.io/akless-components/blank/ccm.blank.js" ],
+      component_obj: [ "ccm.component", "../blank/ccm.blank.js" ],
       times: 3
     },
 
     Instance: function () {
 
-      this.start = callback => {
+      this.start = async () => {
 
         this.element.innerHTML = '';
 
-        let counter = 1;
-        const check = () => --counter === 0 && callback && callback();
-
         for ( let i = 1; i <= this.times; i++ )
-          this.component_obj.start( instance => { this.element.appendChild( instance.root ); check(); } );
-
-        check();
+          this.element.appendChild( ( await this.component_obj.start() ).root );
 
       }
 
@@ -36,5 +32,5 @@
 
   };
 
-  function p(){window.ccm[v].component(component)}const f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{const n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{const e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
-}
+  let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||["latest"])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){window.ccm[c].component(component);document.head.removeChild(a)};a.src=component.ccm.url}
+} )();
