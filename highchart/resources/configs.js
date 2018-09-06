83fb8a8e5444e92eebd998f103193cbf3baaa96c
akless <andre.kless@web.de>
2018-09-06 18:13:20 +0200

    index.html is showcase and local.html uses local paths

--- highchart/resources/configs.js
+++ highchart/resources/configs.js
@@ -5,10 +5,43 @@
  */
 
 ccm.files[ 'configs.js' ] = {
-  "line": {
+  "local": {
     "css": [ "ccm.load", "../highchart/resources/default.css" ],
     "libs": [ "ccm.load", [ "https://code.highcharts.com/highcharts.js", "https://code.highcharts.com/modules/series-label.js", "https://code.highcharts.com/modules/exporting.js" ] ],
-    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
+    "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
+    "chart": "line",
+    "switcher": true,
+    "title": "Solar Employment Growth by Sector, 2010-2016",
+    "subtitle": "Source: thesolarfoundation.com",
+    "y_title": "Number of Employees",
+    "point_start": 2010,
+    "data": [
+      {
+        "name": "Installation",
+        "data": [ 43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175 ]
+      },
+      {
+        "name": "Manufacturing",
+        "data": [ 24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434 ]
+      },
+      {
+        "name": "Sales & Distribution",
+        "data": [ 11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387 ]
+      },
+      {
+        "name": "Project Development",
+        "data": [ null, null, 7988, 12169, 15112, 22452, 34400, 34227 ]
+      },
+      {
+        "name": "Other",
+        "data": [ 12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111 ]
+      }
+    ]
+  },
+  "line": {
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/highchart/resources/default.css" ],
+    "libs": [ "ccm.load", [ "https://code.highcharts.com/highcharts.js", "https://code.highcharts.com/modules/series-label.js", "https://code.highcharts.com/modules/exporting.js" ] ],
+    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.1.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
     "chart": "line",
     "switcher": true,
     "title": "Solar Employment Growth by Sector, 2010-2016",
@@ -39,9 +72,9 @@ ccm.files[ 'configs.js' ] = {
     ]
   },
   "area": {
-    "css": [ "ccm.load", "../highchart/resources/default.css" ],
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/highchart/resources/default.css" ],
     "libs": [ "ccm.load", [ "https://code.highcharts.com/highcharts.js", "https://code.highcharts.com/modules/exporting.js" ] ],
-    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
+    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.1.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
     "chart": "area",
     "switcher": true,
     "title": "US and USSR nuclear stockpiles",
@@ -73,7 +106,7 @@ ccm.files[ 'configs.js' ] = {
     ]
   },
   "bar": {
-    "css": [ "ccm.load", "../highchart/resources/default.css" ],
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/highchart/resources/default.css" ],
     "libs": [ "ccm.load", [ "https://code.highcharts.com/highcharts.js", "https://code.highcharts.com/modules/exporting.js" ] ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.1.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
     "chart": "bar",
@@ -99,7 +132,7 @@ ccm.files[ 'configs.js' ] = {
     ]
   },
   "column": {
-    "css": [ "ccm.load", "../highchart/resources/default.css" ],
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/highchart/resources/default.css" ],
     "libs": [ "ccm.load", [ "https://code.highcharts.com/highcharts.js", "https://code.highcharts.com/modules/exporting.js" ] ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.1.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
     "chart": "column",
@@ -129,7 +162,7 @@ ccm.files[ 'configs.js' ] = {
     ]
   },
   "pie": {
-    "css": [ "ccm.load", "../highchart/resources/default.css" ],
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/highchart/resources/default.css" ],
     "libs": [ "ccm.load", [ "https://code.highcharts.com/highcharts.js", "https://code.highcharts.com/modules/exporting.js" ] ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.1.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
     "chart": "pie",
@@ -166,7 +199,7 @@ ccm.files[ 'configs.js' ] = {
     ]
   },
   "pie-semi-circle": {
-    "css": [ "ccm.load", "../highchart/resources/default.css" ],
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/highchart/resources/default.css" ],
     "libs": [ "ccm.load", [ "https://code.highcharts.com/highcharts.js", "https://code.highcharts.com/modules/exporting.js" ] ],
     "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.1.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
     "chart": "pie-semi-circle",
