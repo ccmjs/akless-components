8314d255e67865f73d740011c1a37d49f75cd2ab
akless <andre.kless@web.de>
2018-09-08 23:52:29 +0200

    ccm.content.js v5.0.0: uses ccm v18.0.0; removed privatization of instance members

--- content/versions/ccm.content-5.0.0.js
+++ content/versions/ccm.content-5.0.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for rendering a predefined content
  * @author André Kless <andre.kless@web.de> 2016-2018
  * @license The MIT License (MIT)
- * @version latest (5.0.0)
+ * @version 5.0.0
  * @changes
  * version 5.0.0 (08.09.2018):
  * - uses ccm v18.0.0
@@ -34,7 +34,9 @@
 
     name: 'content',
 
-    ccm: 'https://ccmjs.github.io/ccm/ccm.js',
+    version: [ 5, 0, 0 ],
+
+    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-18.0.0.js',
 
     config: {
 
