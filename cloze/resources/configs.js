ecfc36acf1b109cbe6ff2f491730b044d9d24a30
akless <andre.kless@web.de>
2018-09-27 10:48:56 +0200

    added old config for cloze demo

--- cloze/resources/configs.js
+++ cloze/resources/configs.js
@@ -85,6 +85,34 @@ ccm.files[ 'configs.js' ] = {
     }
   },
 
+  "demo_old": {
+    "key": "demo_old",
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/cloze/resources/default.css" ],
+    "data": {
+      "store": [ "ccm.store", { "store": "cloze_results", "url": "https://ccm2.inf.h-brs.de", "method": "POST" } ],
+      "key": "demo"
+    },
+    "feedback": true,
+    "time": 300,
+    "keywords": [ "convenience", "conducting", "objectives", "durable", "competitive", "breakdown", "reasons", "evaluate", "adding", "breakthroughs", "withdraw", "patterns", "non-durable", "deleting", "feasible", "making", "sources", "niche" ],
+    "text": "<ol><li>To stay competitive companies must *evaluate* their existing product line and make decisions about *deleting* or *adding* new products.</li><li>Innovation can have different *sources* e.g. “Discontinuous” innovation, which can change existing consumption *patterns*.</li><li>Innovations are of extreme importance for organizations; some innovations are caused by technical *breakthroughs*.</li><li>In order to be successful companies may need to look for a market *niche*.</li><li> It is assumed that *durable* goods last more than one year. *Non-durable*  goods are tangible but provide benefits only for a short period of time. *convenience* products are goods that consumers buy frequently like soft drinks, newspapers etc.</li><li>A business model identifies such things as *competitive* advantage, and how to become profitable. Some business models may not be *feasible* any longer.</li><li>One way to evaluate a product is by *conducting* a discrimination test.</li><li>When a product fails i.e. it does not meet the *objectives* that were set by the organization, the company may be forced to *withdraw* it from the market, as was the case with Walmart in Germany.</li></ol>",
+    "blank": true,
+    "retry": true,
+    "captions.finish": "Save and Restart",
+    "onfinish": {
+      "store": {
+        "settings": {
+          "url": "https://ccm2.inf.h-brs.de",
+          "method": "POST",
+          "store": "cloze_results"
+        },
+        "key": "demo"
+      },
+      "alert": "Saved!",
+      "restart": true
+    }
+  },
+
   "lea": {
     "key": "lea",
     "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/cloze/resources/lea.css" ],
