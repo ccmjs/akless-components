088ce672af7c475b2adaf3270f770720e8e4ac45
akless <andre.kless@web.de>
2018-10-15 22:17:06 +0200

    ccm.quiz.js v3.0.0 - uses ccm v18.0.0 - removed privatization of instance members - updated event handling and logging - added getValue method

--- quiz/versions/ccm.quiz-3.0.0.js
+++ quiz/versions/ccm.quiz-3.0.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for quiz
  * @author André Kless <andre.kless@web.de> 2016-2018
  * @license The MIT License (MIT)
- * @version latest (3.0.0)
+ * @version 3.0.0
  * @changes
  * version 3.0.0 (15.10.2018):
  * - uses ccm v18.0.0
@@ -29,7 +29,9 @@
 
     name: 'quiz',
 
-    ccm: 'https://ccmjs.github.io/ccm/ccm.js',
+    version: [ 3, 0, 0 ],
+
+    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-18.0.6.js',
 
     config: {
 
