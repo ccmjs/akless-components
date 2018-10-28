0e33f1613a39852c351192dfe90ba5d41a27bf8d
akless <andre.kless@web.de>
2018-10-28 20:44:54 +0100

    added Module for conversion of JSON

---
+++ modules/json-to-json.js
@@ -0,0 +1,10 @@
+/**
+ * @overview Module for conversion of JSON
+ * @author André Kless <andre.kless@web.de> 2018
+ * @license The MIT License (MIT)
+ */
+
+export function example( json ) {
+  // change JSON here
+  return json;
+}
