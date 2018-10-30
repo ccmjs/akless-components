5a12463d6f5a1d073e48b2d67ee0b84d65c0412a
akless <andre.kless@web.de>
2018-10-30 12:42:19 +0100

    changed local configs

--- kanban_card/resources/configs.js
+++ kanban_card/resources/configs.js
@@ -6,8 +6,8 @@
 
 ccm.files[ 'configs.js' ] = {
 
-  "local": {
-    "key": "local",
+  "local_white": {
+    "key": "local_white",
     "css.1": "../kanban_card/resources/default.css",
     "data": {
       "store": [ "ccm.store", "../kanban_card/resources/datasets.js" ],
@@ -15,6 +15,20 @@ ccm.files[ 'configs.js' ] = {
     },
     "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
     "onchange": function ( event ) { console.log( this.index, 'onchange', this.getValue(), event ) }
+  },
+
+  "local_gold": {
+    "key": "local_gold",
+    "css.1": "../kanban_card/resources/gold.css",
+    "data": {
+      "store": [ "ccm.store", "../kanban_card/resources/datasets.js" ],
+      "key": "presentation"
+    }
+  },
+
+  "local_blue": {
+    "key": "local_blue",
+    "css.1": "../kanban_card/resources/blue.css"
   },
 
   "demo_white": {
