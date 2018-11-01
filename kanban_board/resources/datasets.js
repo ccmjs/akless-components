57690dd629ae6635ca2633fd0dba2a955b42a1cf
akless <andre.kless@web.de>
2018-11-01 22:59:04 +0100

    changed experimental dataset

--- kanban_board/resources/datasets.js
+++ kanban_board/resources/datasets.js
@@ -24,46 +24,23 @@ ccm.files[ 'datasets.js' ] = {
     ]
   },
 
-  "showcase": {
-    "key": "showcase",
-    "lanes": [
-      {
-        "cards": [
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/kanban_card/versions/ccm.kanban_card-1.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/kanban_card/resources/configs.js", "test" ] ],
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/kanban_card/versions/ccm.kanban_card-1.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/kanban_card/resources/configs.js", "realtime" ] ]
-        ]
-      },
-      {
-        "cards": [
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/kanban_card/versions/ccm.kanban_card-1.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/kanban_card/resources/configs.js", "w2c" ] ]
-        ]
-      },
-      {
-        "cards": [
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/kanban_card/versions/ccm.kanban_card-1.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/kanban_card/resources/configs.js", "lea" ] ],
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/kanban_card/versions/ccm.kanban_card-1.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/kanban_card/resources/configs.js", "more" ] ]
-        ]
-      }
-    ]
-  },
-
   "experimental": {
     "key": "experimental",
     "lanes": [
       {
         "cards": [
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/kanban_card/versions/ccm.kanban_card-1.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/kanban_card/resources/configs.js", "realtime" ] ]
+          [ "ccm.instance", "../kanban_card/ccm.kanban_card.js", [ "ccm.get", "../kanban_card/resources/configs.js", "local_white" ] ],
         ]
       },
       {
         "cards": [
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/quiz/versions/ccm.quiz-2.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/quiz/resources/configs.js", "demo" ] ]
+          [ "ccm.instance", "../quiz/ccm.quiz.js", [ "ccm.get", "../quiz/resources/configs.js", "local" ] ]
         ]
       },
       {
         "cards": [
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/blank/ccm.blank.js" ],
-          [ "ccm.instance", "https://ccmjs.github.io/akless-components/eval/versions/ccm.eval-1.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/eval/resources/configs.js", "demo" ] ]
+          [ "ccm.instance", "../blank/ccm.blank.js" ],
+          [ "ccm.instance", "../cloze/ccm.cloze.js", [ "ccm.get", "../cloze/resources/configs.js", "local" ] ]
         ]
       }
     ]
