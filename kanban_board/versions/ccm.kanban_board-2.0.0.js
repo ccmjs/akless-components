a3ea733eec77935b1d5bd3399107399328587a41
akless <andre.kless@web.de>
2018-11-02 11:47:25 +0100

    bug fix

--- kanban_board/versions/ccm.kanban_board-2.0.0.js
+++ kanban_board/versions/ccm.kanban_board-2.0.0.js
@@ -81,7 +81,7 @@
         data = await $.dataset( this.data );
 
         // set initial lanes
-        data.lanes = []; for ( let i = 0; i < this.lanes.length; i++ ) if ( !data.lanes[ i ] ) data.lanes[ i ] = { cards: [] };
+        if ( !data.lanes ) data.lanes = []; for ( let i = 0; i < this.lanes.length; i++ ) if ( !data.lanes[ i ] ) data.lanes[ i ] = { cards: [] };
 
         // logging of 'start' event
         this.logger && this.logger.log( 'start', $.clone( data ) );
