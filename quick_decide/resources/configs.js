b18a7154a4eecfcb9f588da5f7661951d149bee3
akless <andre.kless@web.de>
2018-11-15 10:29:35 +0100

    added time limit demo

--- quick_decide/resources/configs.js
+++ quick_decide/resources/configs.js
@@ -7,7 +7,6 @@
 ccm.files[ 'configs.js' ] = {
 
   "local": {
-
     "key": "local",
     "css.1": "../quick_decide/resources/default.css",
     "questions": [
@@ -38,11 +37,9 @@ ccm.files[ 'configs.js' ] = {
     ],
     "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
     "onfinish": { "log": true }
-
   },
 
   "demo": {
-
     "key": "demo",
     "questions": [
       {
@@ -71,7 +68,40 @@ ccm.files[ 'configs.js' ] = {
       }
     ],
     "interval": 1
+  },
 
+  "time_limit": {
+
+    "key": "time_limit",
+    "questions": [
+      {
+        "text": "Question A",
+        "answers": [ "Answer A", "Answer B", "Answer C" ]
+      },
+      {
+        "text": "Which Letter?",
+        "answers": [ "A", "B" ]
+      },
+      {
+        "text": "Which Number?",
+        "answers": [ "1", "2" ]
+      },
+      {
+        "text": "Do you agree?",
+        "answers": [ "Yes", "No" ]
+      },
+      {
+        "text": "Which Fruit",
+        "answers": [ "Apple", "Pear" ]
+      },
+      {
+        "text": "Enough?",
+        "answers": [ "Finish" ]
+      }
+    ],
+    "interval": 1,
+    "limit": 2000,
+    "wait": 1000
   }
 
 };
