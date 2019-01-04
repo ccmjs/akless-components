b8317ce0dfb245caa07229ffde5ab779ff6a3b5b
akless <andre.kless@web.de>
2019-01-04 23:06:40 +0100

    ccm.app_builder.js: update default config

--- app_builder/versions/ccm.app_builder-1.1.2.js
+++ app_builder/versions/ccm.app_builder-1.1.2.js
@@ -211,11 +211,11 @@
         "https://ccmjs.github.io/akless-components/app_builder/resources/default.css"
       ],
       "data": { "store": [ "ccm.store" ] },
-      "warning": "Are you sure you want to delete this App?"
+      "warning": "Are you sure you want to delete this App?",
+      "builder": [ "ccm.component", "https://ccmjs.github.io/akless-components/submit/versions/ccm.submit-6.7.0.js", [ "ccm.get", { "name": "submit", "url": "https://ccm2.inf.h-brs.de" }, "quick_decide_builder" ] ],
+      "app": [ "ccm.component", "https://ccmjs.github.io/akless-components/quick_decide/versions/ccm.quick_decide-1.3.0.js" ]
 
   //  "convert": { "app_to_builder": json => json, "builder_to_app": json => json },
-  //  "builder",
-  //  "app",
   //  "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.2.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "guest" ] ],
   //  "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-4.0.1.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
   //  "onchange"
