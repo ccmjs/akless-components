f656c94dc83674e97700130c7562a19e8082adb5
akless <andre.kless@web.de>
2019-01-16 21:14:33 +0100

    bug fix for ccm.window.js

--- window/ccm.window.js
+++ window/ccm.window.js
@@ -153,11 +153,7 @@
             else if ( elem.msRequestFullscreen )     /* IE/Edge */
               elem.msRequestFullscreen();
           },
-          booklet: $.format( 'javascript:!function(){var%20e=document.createElement(%22script%22);e.setAttribute(%22src%22,%22%url%%22),document.head.appendChild(e),e=document.createElement(%22ccm-%index%%22),e.setAttribute(%22style%22,%22position:absolute;top:0%22),e.setAttribute(%22key%22,%22%config%%22),document.body.appendChild(e)}();', {
-            url: this.component.url || this._url,
-            index: this.component.index,
-            config: encodeURI( $.stringify( $.integrate( { _url: this.component.url }, $.parse( this.config ) ) ).replace( /"/g, '\\"' ) )
-          } ),
+          booklet: this.booklet( { _url: this.component.url || this._url } ),
           close: () => $.removeElement( this.root.parentNode )
         } ) );
 
@@ -206,7 +202,7 @@
        * @param {Object} [config] - priority data for instance configuration
        * @returns {string} booklet
        */
-      this.booklet = config => $.format( 'javascript:!function(){var%20e=document.createElement(%22script%22);e.setAttribute(%22src%22,%22%url%%22),document.head.appendChild(e),e=document.createElement(%22ccm-%index%%22),e.setAttribute(%22key%22,%22%config%%22),document.body.appendChild(e)}();', {
+      this.booklet = config => $.format( 'javascript:!function(){var%20e=document.createElement(%22script%22);e.setAttribute(%22src%22,%22%url%%22),document.head.appendChild(e),e=document.createElement(%22ccm-%index%%22),e.setAttribute(%22style%22,%22position:absolute;top:0%22),e.setAttribute(%22key%22,%22%config%%22),document.body.appendChild(e)}();', {
         url: this.component.url,
         index: this.component.index,
         config: encodeURI( ( config ? $.stringify( $.integrate( config, $.parse( this.config ) ) ) : this.config ).replace( /"/g, '\\"' ) )
