d9262f46d0d392f64543d99e6e8830c2e7d20a5f
akless <andre.kless@web.de>
2019-01-20 23:38:56 +0100

    use of local datastore in local config

--- store/resources/configs.js
+++ store/resources/configs.js
@@ -9,10 +9,6 @@ ccm.files[ 'configs.js' ] = {
   "local": {
     "key": "local",
     "css": [ "ccm.load", "../store/resources/default.css", "../libs/bootstrap-4/css/bootstrap.min.css" ],
-    "data": {
-      "store": [ "ccm.store", { "name": "test", "url": "https://ccm2.inf.h-brs.de" } ],
-      "key": {}
-    },
     "builder.1": "../json_builder/ccm.json_builder.js",
 //  "user": [ "ccm.instance", "../user/ccm.user.js", [ "ccm.get", "../user/resources/configs.js", "local" ] ],
     "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ]
