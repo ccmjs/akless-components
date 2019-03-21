b9c7057705c114b92d01a4b67c9d0698d3a06027
akless <andre.kless@web.de>
2019-03-21 13:26:32 +0100

    added module function

--- modules/json-to-json.js
+++ modules/json-to-json.js
@@ -35,3 +35,53 @@ export function poll_to_plotly( poll ) {
   };
 
 }
+
+/**
+ * converts member votes of a live poll to a Highchart.js bar chart configuration
+ * @param {Object} poll - member votings of a live poll
+ * @returns {Object} Highchart.js bar chart configuration
+ */
+export function poll_to_highchart( poll ) {
+
+  const data = [];
+  Object.values( poll ).forEach( value => {
+    if ( isNaN( data[ value ] ) )
+      data[ value - 1 ] = 1;
+    else
+      data[ value - 1 ]++;
+  } );
+
+  const categories = [];
+  for ( let i = 0; i < data.length; i++ )
+    categories[ i ] = String.fromCharCode( 65 + i );
+
+  return {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: ''
+    },
+    xAxis: {
+      categories: categories,
+      crosshair: true
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Votes'
+      },
+      tickInterval: 1
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0
+      }
+    },
+    legend: false,
+    tooltip: false,
+    series: [ { data: data } ]
+  };
+
+}
