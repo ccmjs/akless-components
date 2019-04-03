13ee477e844ac915de64f414600adb5cae4ae5b6
akless <andre.kless@web.de>
2019-04-03 13:27:31 +0200

    demo configuration uses ccm.table.js v2.1.0

--- show_solutions/resources/configs.js
+++ show_solutions/resources/configs.js
@@ -9,10 +9,10 @@ ccm.files[ 'configs.js' ] = {
   "localhost": {
     "key": "localhost",
     "data": {
-      "store": [ "ccm.store", { "url": "http://localhost:8080", "store": "show_solutions", "method": "POST" } ],
-      "key": "{}"
+      "store": [ "ccm.store", { "url": "http://localhost:8080", "name": "show_solutions" } ],
+      "key": {}
     },
-    "target": [ "ccm.component", "https://ccmjs.github.io/tkless-components/table/versions/ccm.table-1.0.0.js" ]
+    "target": [ "ccm.component", "https://ccmjs.github.io/tkless-components/table/versions/ccm.table-2.1.0.js" ]
   },
 
   "we_ss18": {
@@ -21,7 +21,7 @@ ccm.files[ 'configs.js' ] = {
       "store": [ "ccm.store", { "url": "https://ccm2.inf.h-brs.de", "name": "we_ss18_solutions" } ],
       "key": { "_id": { "$regex": ",le02_a1$" } }
     },
-    "target": [ "ccm.component", "https://ccmjs.github.io/tkless-components/table/versions/ccm.table-1.0.0.js" ]
+    "target": [ "ccm.component", "https://ccmjs.github.io/tkless-components/table/versions/ccm.table-2.1.0.js" ]
   }
 
 };
