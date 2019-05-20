8db6f513ccc24976289dcb56308801f05ec68e53
akless <andre.kless@web.de>
2019-05-20 09:34:33 +0200

    ccm.routing.js v1.1.0: added this.get(): returns current app route of location hash (route name and parameters)

--- routing/versions/ccm.routing-1.1.0.js
+++ routing/versions/ccm.routing-1.1.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for routing
  * @author André Kless <andre.kless@web.de> 2019
  * @license MIT License
- * @version latest (1.1.0)
+ * @version 1.1.0
  * @changes
  * version 1.1.0 (20.05.2019):
  * - added this.get(): returns current app route of location hash (route name and parameters)
@@ -13,9 +13,9 @@
 
   const component = {
 
-    name: 'routing',
+    name: 'routing', version: [ 1, 1, 0 ],
 
-    ccm: 'https://ccmjs.github.io/ccm/ccm.js',
+    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-20.6.2.js',
 
     config: {
 //    "app": "1558132111384X2108359471753687"
