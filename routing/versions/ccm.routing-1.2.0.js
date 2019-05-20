766bde1e6bafa9d54931cd6cf432b4269b848e78
akless <andre.kless@web.de>
2019-05-20 10:58:12 +0200

    ccm v1.2.0: optional automatic generation of an app ID (config.app = true)

--- routing/versions/ccm.routing-1.2.0.js
+++ routing/versions/ccm.routing-1.2.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for routing
  * @author André Kless <andre.kless@web.de> 2019
  * @license MIT License
- * @version latest (1.2.0)
+ * @version 1.2.0
  * @changes
  * version 1.2.0 (20.05.2019):
  * - optional automatic generation of an app ID (config.app = true)
@@ -15,9 +15,9 @@
 
   const component = {
 
-    name: 'routing',
+    name: 'routing', version: [ 1, 2, 0 ],
 
-    ccm: 'https://ccmjs.github.io/ccm/ccm.js',
+    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-20.6.2.js',
 
     config: {
 //    "app": "1558132111384X2108359471753687",
