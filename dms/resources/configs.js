850f15e2e4d2308b777e887d8cd9bda91b8dc1f4
akless <andre.kless@web.de>
2019-05-21 14:14:14 +0200

    updated DMS resources

--- dms/resources/configs.js
+++ dms/resources/configs.js
@@ -1,6 +1,6 @@
 /**
  * @overview configurations of ccm component for digital maker space
- * @author André Kless <andre.kless@web.de> 2018
+ * @author André Kless <andre.kless@web.de> 2018-2019
  * @license The MIT License (MIT)
  */
 
@@ -8,76 +8,51 @@ ccm.files[ 'configs.js' ] = {
 
   "local": {
     "key": "local",
-    "css": [ "ccm.load",
-      "../libs/bootstrap/css/bootstrap.css",
-      { "context": "head", "url": "../libs/bootstrap/css/font-face.css" },
-      "../dms/resources/default.css"
-    ],
+    "css.1": "../dms/resources/default.css",
     "data": {
-      "store": [ "ccm.store", "../dms/resources/datasets.js" ],
+      "store": [ "ccm.store", { "name": "dms-components", "url": "http://localhost:8080" } ],
       "key": {}
     },
+    "menu.1": "../menu/ccm.menu.js",
+    "menu.2.routing.1": "../routing/ccm.routing.js",
     "listing.1": "../listing/ccm.listing.js",
     "listing.2.css.1": "../dms/resources/listing.css",
     "listing.2.defaults.icon": "../dms/resources/component.png",
-    "rating.2.css.3": "../dms/resources/rating_result.css",
+    "rating.2.data": { "store": [ "ccm.store", [ "ccm.get", { "name": "dms-component_ratings", "url": "http://localhost:8080" }, {} ] ] },
     "form.1": "../submit/ccm.submit.js",
     "form.2.css": [ "ccm.load",
+      { "context": "head", "url": "../libs/bootstrap/css/font-face.css" },
       "../libs/bootstrap/css/bootstrap.css",
-      "../dms/resources/submit.css"
+      "../submit/resources/default.css"
     ],
+    "form.2.entries.1": "../dms/resources/publish_form.js",
+    "form.2.data": {
+      "store": [ "ccm.store", { "name": "dms-components", "url": "http://localhost:8080" } ]
+    },
     "component_manager": [ "ccm.component", "../component_manager/ccm.component_manager.js", [ "ccm.get", "../component_manager/resources/configs.js", "local" ] ],
-    "user": [ "ccm.instance", "../user/ccm.user.js", {
-      "key": [ "ccm.get", "../user/resources/configs.js", "local" ],
-      "html.logged_in.class": "",
-      "html.logged_out.class": ""
-    } ],
-    "logger":  [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ]
-  },
-
-  "developer": {
-    "key": "developer",
-    "css": [ "ccm.load",
+    "user.1": "../user/ccm.user.js",
+    "user.2.css": [ "ccm.load",
       "../libs/bootstrap/css/bootstrap.css",
       { "context": "head", "url": "../libs/bootstrap/css/font-face.css" },
-      "../dms/resources/default.css"
+      "../user/resources/default.css"
     ],
-    "data": {
-      "store": [ "ccm.store", { "name": "components", "url": "https://ccm2.inf.h-brs.de" } ],
-      "key": {}
-    },
-    "listing.1": "../listing/ccm.listing.js",
-    "listing.2.css.1": "../dms/resources/listing.css",
-    "listing.2.defaults.icon": "../dms/resources/component.png",
-    "rating.2.css.3": "../dms/resources/rating_result.css",
-    "rating.2.data": { "store": [ "ccm.store", [ "ccm.get", { "name": "component_ratings", "url": "https://ccm2.inf.h-brs.de" }, {} ] ] },
-    "form.1": "../submit/ccm.submit.js",
-    "form.2.css": [ "ccm.load",
-      "../libs/bootstrap/css/bootstrap.css",
-      "../dms/resources/submit.css"
-    ],
-    "component_manager": [ "ccm.component", "../component_manager/ccm.component_manager.js", [ "ccm.get", "../component_manager/resources/configs.js", "developer" ] ],
-    "user": [ "ccm.instance", "../user/ccm.user.js", {
-      "key": [ "ccm.get", "../user/resources/configs.js", "local" ],
-      "html.logged_in.class": "",
-      "html.logged_out.class": ""
-    } ],
-    "logger":  [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ]
+    "logger":  [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
+    "routing.1": "../routing/ccm.routing.js",
+    "logo": "../dms/resources/component.png"
   },
 
   "live": {
     "key": "live",
+    "css.1": "https://ccmjs.github.io/akless-components/dms/resources/default-2.0.0.css",
     "data": {
-      "store": [ "ccm.store", { "name": "components", "url": "https://ccm2.inf.h-brs.de" } ],
+      "store": [ "ccm.store", { "name": "dms-components", "url": "https://ccm2.inf.h-brs.de" } ],
       "key": {}
     },
-    "rating.2.data": { "store": [ "ccm.store", [ "ccm.get", { "name": "component_ratings", "url": "https://ccm2.inf.h-brs.de" }, {} ] ] },
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.3.1.js", {
-      "key": [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "guest" ],
-      "html.logged_in.class": "",
-      "html.logged_out.class": ""
-    } ]
-
+    "rating.2.data": { "store": [ "ccm.store", [ "ccm.get", { "name": "dms-component_ratings", "url": "https://ccm2.inf.h-brs.de" }, {} ] ] },
+    "form.2.data": {
+      "store": [ "ccm.store", { "name": "dms-components", "url": "https://ccm2.inf.h-brs.de" } ]
+    },
+    "routing.2.app": "live"
   }
 
 };
