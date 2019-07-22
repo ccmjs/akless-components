e3557975d236a55dfd05dd40107607cf023ad69d
akless <andre.kless@web.de>
2019-07-22 13:12:24 +0200

    bug fix

--- dms/resources/resources.js
+++ dms/resources/resources.js
@@ -7,7 +7,7 @@
 ccm.files[ 'resources.js' ] = {
 
   "local": {
-    "css": [ "ccm.load", "../dms/resources/img/dms.css" ],
+    "css": [ "ccm.load", "../dms/resources/css/dms.css" ],
     "component_manager": [ "ccm.component", "../component_manager/ccm.component_manager.js", {
       "key": [ "ccm.get", "../component_manager/resources/resources.js", "local" ],
       "routing": [ "ccm.instance", "../routing/ccm.routing.js", { "app": "section" } ],
