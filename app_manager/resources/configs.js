2f77ad2b8d5a0b1d2f0dce05c38200c745cc0d36
akless <andre.kless@web.de>
2019-07-26 20:38:59 +0200

    added ccm component for managing an app (ccm.app_manager.js)

---
+++ app_manager/resources/configs.js
@@ -0,0 +1,46 @@
+/**
+ * @overview configurations of ccm component for app manager
+ * @author André Kless <andre.kless@web.de> 2019
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ 'configs.js' ] = {
+
+  "local": {
+    "css.1": "../app_manager/resources/styles.css",
+    "data": {
+      "store": [ "ccm.store", { "url": "https://ccm2.inf.h-brs.de", "name": "dms-apps" } ],
+      "key": "1562615085075X31075165857521436"
+    },
+    "default_icon": "../dms/resources/img/default.png",
+    "handover_app": [ "ccm.component", "../handover_app/ccm.handover_app.js", {
+      "css": [ "ccm.load",
+        "../handover_app/resources/styles.css",
+        "../libs/bootstrap-4/css/bootstrap.min.css",
+        { "context": "head", "url": "../libs/bootstrap-4/css/bootstrap.min.css" }
+      ],
+      "helper.1": "../modules/helper.mjs",
+      "html.1": "../handover_app/resources/template.html",
+      "qr_code": [ "ccm.load", "../libs/qrcode-generator/qrcode.min.js" ],
+      "window": [ "ccm.component", "../window/ccm.window.js" ]
+    } ],
+    "helper.1": "../modules/helper.mjs",
+    "html.1": "../app_manager/resources/templates.html",
+    "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
+    "user": [ "ccm.start", "../user/ccm.user.js", [ "ccm.get", "../user/resources/configs.js", "guest" ] ]
+  },
+
+  "demo": {
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/app_manager/resources/styles.css" ],
+    "data": {
+      "store": [ "ccm.store", { "url": "https://ccm2.inf.h-brs.de", "name": "dms-apps" } ],
+      "key": "1562615085075X31075165857521436"
+    },
+    "handover_app": [ "ccm.component", "https://ccmjs.github.io/akless-components/handover_app/versions/ccm.handover_app-1.0.0.js", {
+      "qr_code": [ "ccm.load", "https://ccmjs.github.io/akless-components/libs/qrcode-generator/qrcode.min.js" ],
+      "window": [ "ccm.component", "https://ccmjs.github.io/akless-components/window/versions/ccm.window-1.0.0.js" ]
+    } ],
+    "user": [ "ccm.start", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.2.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "guest" ] ]
+  }
+
+};
---
+++ app_manager/versions/ccm.app_manager-1.0.0.js
@@ -0,0 +1,95 @@
+/**
+ * @overview ccm component for managing an app
+ * @author André Kless <andre.kless@web.de> 2019
+ * @license MIT License
+ * @version 1.0.0
+ * @changes
+ * version 1.0.0 (26.07.2019)
+ */
+
+( () => {
+
+  const component = {
+
+    name: 'app_manager', version: [ 1, 0, 0 ],
+
+    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-22.2.1.js',
+
+    config: {
+      "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/app_manager/resources/styles.css" ],
+//    "component_manager": [ "ccm.component", "https://ccmjs.github.io/akless-components/component_manager/ccm.component_manager-3.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/component_manager/resources/resources.js", "demo" ] ],
+      "data": { "store": [ "ccm.store" ] },
+      "default_icon": "https://ccmjs.github.io/akless-components/dms/resources/img/default.png",
+//    "handover_app": [ "ccm.component", "https://ccmjs.github.io/akless-components/handover_app/versions/ccm.handover_app-1.0.0.js" ],
+      "helper": [ "ccm.load", "https://ccmjs.github.io/akless-components/modules/helper.mjs" ],
+      "html": [ "ccm.load", "https://ccmjs.github.io/akless-components/app_manager/resources/templates.html" ],
+//    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-4.0.2.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
+//    "rating": [ "ccm.component", ... },
+//    "user": [ "ccm.start", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.2.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "guest" ] ]
+    },
+
+    Instance: function () {
+
+      let $;
+
+      this.ready = async () => {
+
+        // set shortcut to help functions
+        $ = this.ccm.helper;
+
+        // logging of 'ready' event
+        this.logger && this.logger.log( 'ready', $.privatize( this, true ) );
+
+      };
+
+      this.start = async () => {
+
+        // get app dataset
+        const dataset = await $.dataset( this.data );
+
+        // adjust app dataset
+        if ( !dataset.icon ) dataset.icon = this.default_icon;
+        dataset.tags = dataset.tags.join( ', ' );
+        if ( dataset.language ) dataset.language = dataset.language.toUpperCase();
+
+        // logging of 'start' event
+        this.logger && this.logger.log( 'start', $.clone( dataset ) );
+
+        // render main HTML structure
+        $.setContent( this.element, $.html( this.html.main, $.integrate( $.clone( dataset ), {
+          create_similar_app: () => {},
+          fullscreen: () => this.helper.fullscreen( this.element.querySelector( '#app' ) )
+        } ) ) );
+
+        // remove no needed areas
+        if ( !this.component_manager ) $.removeElement( this.element.querySelector( '#button'      ) );
+        if ( !dataset.abstract       ) $.removeElement( this.element.querySelector( '#abstract'    ) );
+        if ( !dataset.description    ) $.removeElement( this.element.querySelector( '#description' ) );
+
+        // render app
+        await this.ccm.start( dataset.path, {
+          key: [ 'ccm.get', dataset.source[ 0 ], dataset.source[ 1 ] ],
+          parent: this,
+          root: this.element.querySelector( '#app' )
+        } );
+
+        // render handover of the app
+        if ( this.handover_app )
+          await this.handover_app.start( {
+            root: this.element.querySelector( '#handover' ),
+            component_url: dataset.path,
+            data: {
+              store: [ "ccm.store", dataset.source[ 0 ] ],
+              key: dataset.source[ 1 ]
+            }
+          } );
+        else $.removeElement( this.element.querySelector( '#section-handover' ) );
+
+      };
+
+    }
+
+  };
+
+  let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||["latest"])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){window.ccm[c].component(component);document.head.removeChild(a)};a.src=component.ccm.url}
+} )();
