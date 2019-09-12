34361b7ef391a4703655cb8f56f1f4e7a488b08e
akless <andre.kless@web.de>
2019-09-12 09:50:37 +0200

    added submit config for edit component metadata

--- submit/resources/configs.js
+++ submit/resources/configs.js
@@ -151,6 +151,102 @@ ccm.files[ 'configs.js' ] = {
       },
       { "type": "submit" }
     ]
+  },
+
+  "component_meta": {
+    "entries": [
+      "<div class='page-header'><h3>Edit of Published Component</h3></div>",
+      "<div class='well'>The more information about the published component you type here, the better your component can be found and reused by others.</div>",
+      {
+        "label": "<span style='color:red'>*</span>Title",
+        "name": "title",
+        "type": "text",
+        "info": "Title of the component. As understandable and short as possible.",
+        "placeholder": "Digital Makerspace",
+        "required": true,
+        "maxlength": 35
+      },
+      {
+        "label": "<span style='color:red'>*</span>URL",
+        "name": "path",
+        "type": "url",
+        "info": "Your component file must be accessible via a public URL on the web. Most developers use <a href='https://pages.github.com/' target='_blank'>GitHub Pages</a> for this. For the transparency please do not publish minimized code. Never publish the latest version of your component, but always a concrete version whose code does not change anymore.",
+        "placeholder": "https://ccmjs.github.io/akless-components/dms/versions/ccm.dms-2.0.0.js",
+        "required": true,
+        "pattern": ".+/ccm\\.([a-z][a-z0-9_]*)(-(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*))(\\.js)$",
+        "title": "The filename of the component must start with 'ccm.' followed by the unique component name and then a '-' followed by the component version number and a '.js' in the end. Example: ccm.dms-1.0.0.js"
+      },
+      {
+        "label": "<span style='color:red'>*</span>SRI Hash",
+        "name": "sri",
+        "type": "text",
+        "info": "To guarantee that the code of your component does not change, we need the component's so-called SRI hash. The required SRI hash can be generated via a <a href='https://www.srihash.org/' target='_blank'>free online tool</a>. More information can be found on the Web under the keyword <a href='https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity' target='_blank'>Subresource Integrity</a>.",
+        "placeholder": "sha384-eBCG79vlakZs8qCC6yARu2oQSMB946XWO/x4uhwpGFl9AQZllpXZdqGq/3f2Mu9N",
+        "required": true,
+        "pattern": "sha384-.+",
+        "title": "An SRI hash always starts with \"sha384-\"."
+      },
+      {
+        "label": "Icon",
+        "name": "icon",
+        "type": "url",
+        "info": "The icon file must be accessible via a public URL on the web and is ideally a 64x64 SVG file. The icon will be published under the <a href='https://creativecommons.org/share-your-work/public-domain/cc0/' target='_blank'>CC0 license</a>. Make sure the icon is compatible with this license.",
+        "placeholder": "https://ccmjs.github.io/akless-components/dms/resources/img/component.png"
+      },
+      {
+        "label": "Abstract",
+        "name": "subject",
+        "type": "text",
+        "info": "A short description of your component. Ideally in title case",
+        "placeholder": "Provides Components for Creating Apps.",
+        "maxlength": 70
+      },
+      {
+        "label": "Description",
+        "name": "description",
+        "type": "textarea",
+        "info": "A detailed description of your component. Try to make as understandable as possible what kind of apps you can create with the help of your component.",
+        "placeholder": "Enables you to create your own Digital Makerspace, where component developers can publish, find, try, and rate components, and app creators can create their own apps from components without programming skills. With the built-in App Store, the created apps can be found, tried, rated, reused and shared by others. Everything in a Digital Makerspace is free software and all content is public domain."
+      },
+      {
+        "label": "Category",
+        "name": "category",
+        "type": "radio",
+        "info": "What kind of component is it? Choose the category that best fits the type of your component.<br>In the case of <code>App Creation</code>, the component is provided as a digital tool for creating apps.<br>The component can be used as an <code>Configuration Builder</code> if an app created from the component can be used to create an app configuration.<br>A <code>Utility</code> component is not for creating apps, but provides useful functionality that can be optionally used by other apps.",
+        "items": [
+          {
+            "label": "App Creation",
+            "value": "App Creation",
+            "checked": true
+          },
+          {
+            "label": "Configuration Builder",
+            "value": "Configuration Builder"
+          },
+          {
+            "label": "Utility",
+            "value": "Utility"
+          }
+        ]
+      },
+      {
+        "label": "Tags",
+        "type": "several",
+        "info": "Here you can define any tags through which your component can be found. Only one tag per input field. Via +/- you can add/remove additional input fields.",
+        "item": {
+          "name": "tags",
+          "type": "text"
+        }
+      },
+      "<div class='well'><p>I agree that all software of my app is released as free software under the <a href='https://en.wikipedia.org/wiki/MIT_License' target='_blank'>MIT license</a>.</p><p>I agree that all content of my app will be released as public domain under the <a href='https://creativecommons.org/share-your-work/public-domain/cc0/' target='_blank'>CC0 license</a>.</p>I confirm that this does not violate the copyright of third parties.</div>",
+      {
+        "label": "<span style='color:red'>*</span>I Agree",
+        "type": "checkbox",
+        "info": "Everything in a Digital Makerspace is free software and all content is public domain. So you can only publish anything if this requirement is fulfilled.",
+        "required": true
+      },
+      { "type": "submit" }
+    ]
   }
 
 };
