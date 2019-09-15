ef22d3da8c6df924328bbbb0d971c664cba93666
akless <andre.kless@web.de>
2019-09-15 13:32:32 +0200

    ccm.menu.js v2.8.1: bug fix for menu entry ID and routing; uses ccm v22.6.0

--- menu/versions/ccm.menu-2.8.1.js
+++ menu/versions/ccm.menu-2.8.1.js
@@ -2,7 +2,7 @@
  * @overview ccm component for rendering a menu
  * @author André Kless <andre.kless@web.de> 2015-2016, 2018-2019
  * @license The MIT License (MIT)
- * @version latest (2.8.1)
+ * @version 2.8.1
  * @changes
  * version 2.8.1 (15.09.2019):
  * - bug fix for menu entry ID and routing
@@ -56,7 +56,7 @@
 
   const component = {
 
-    name: 'menu',
+    name: 'menu', version: [ 22, 8, 1 ],
 
     ccm: 'https://ccmjs.github.io/ccm/versions/ccm-22.6.0.js',
 
