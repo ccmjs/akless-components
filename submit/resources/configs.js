cdfe2365735b7cfe9f5bef4e37627bc2b040e1d1
akless <andre.kless@web.de>
2019-09-17 12:11:50 +0200

    separate ccm.submit.js configs for edit and publish component

--- submit/resources/configs.js
+++ submit/resources/configs.js
@@ -153,9 +153,9 @@ ccm.files[ 'configs.js' ] = {
     ]
   },
 
-  "component_meta": {
+  "component_meta_edit": {
     "entries": [
-      "<div class='page-header'><h3>Edit of Published Component</h3></div>",
+      "<div class='page-header'><h3>Edit of Published Component%</h3></div>",
       "<div class='well'>The more information about the published component you type here, the better your component can be found and reused by others.</div>",
       {
         "label": "<span style='color:red'>*</span>Title",
@@ -296,6 +296,65 @@ ccm.files[ 'configs.js' ] = {
             "required": true
           }
         ]
+      },
+      "<div class='well'><p>I agree that all software of my app is released as free software under the <a href='https://en.wikipedia.org/wiki/MIT_License' target='_blank'>MIT license</a>.</p><p>I agree that all content of my app will be released as public domain under the <a href='https://creativecommons.org/share-your-work/public-domain/cc0/' target='_blank'>CC0 license</a>.</p>I confirm that this does not violate the copyright of third parties.</div>",
+      {
+        "label": "<span style='color:red'>*</span>I Agree",
+        "type": "checkbox",
+        "info": "Everything in a Digital Makerspace is free software and all content is public domain. So you can only publish anything if this requirement is fulfilled.",
+        "required": true
+      },
+      { "type": "submit" }
+    ]
+  },
+
+  "component_meta_publish": {
+    "entries": [
+      "<div class='page-header'><h3>Publish Component</h3></div>",
+      "<div class='well'>Are you a component developer? Have you developed a useful component that you would like to offer to the world? Great! Then you can use this form to publish your component. The more information about the published component you type here, the better your component can be found and reused by others.</div>",
+      {
+        "label": "<span style='color:red'>*</span>Title",
+        "name": "title",
+        "type": "text",
+        "info": "Title of the component. As understandable and short as possible.",
+        "placeholder": "Digital Makerspace",
+        "required": true,
+        "maxlength": 35
+      },
+      {
+        "label": "<span style='color:red'>*</span>URL",
+        "name": "path",
+        "type": "url",
+        "info": "Your component file must be accessible via a public URL on the web. Most developers use <a href='https://pages.github.com/' target='_blank'>GitHub Pages</a> for this. For the transparency please do not publish minimized code. Never publish the latest version of your component, but always a concrete version whose code does not change anymore.",
+        "placeholder": "https://ccmjs.github.io/akless-components/dms/versions/ccm.dms-2.0.0.js",
+        "required": true,
+        "pattern": ".+/ccm\\.([a-z][a-z0-9_]*)(-(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*))(\\.js)$",
+        "title": "The filename of the component must start with 'ccm.' followed by the unique component name and then a '-' followed by the component version number and a '.js' in the end. Example: ccm.dms-1.0.0.js"
+      },
+      {
+        "label": "<span style='color:red'>*</span>SRI Hash",
+        "name": "sri",
+        "type": "text",
+        "info": "To guarantee that the code of your component does not change, we need the component's so-called SRI hash. The required SRI hash can be generated via a <a href='https://www.srihash.org/' target='_blank'>free online tool</a>. More information can be found on the Web under the keyword <a href='https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity' target='_blank'>Subresource Integrity</a>.",
+        "placeholder": "sha384-eBCG79vlakZs8qCC6yARu2oQSMB946XWO/x4uhwpGFl9AQZllpXZdqGq/3f2Mu9N",
+        "required": true,
+        "pattern": "sha384-.+",
+        "title": "An SRI hash always starts with \"sha384-\"."
+      },
+      {
+        "label": "Icon",
+        "name": "icon",
+        "type": "url",
+        "info": "The icon file must be accessible via a public URL on the web and is ideally a 64x64 SVG file. The icon will be published under the <a href='https://creativecommons.org/share-your-work/public-domain/cc0/' target='_blank'>CC0 license</a>. Make sure the icon is compatible with this license.",
+        "placeholder": "https://ccmjs.github.io/akless-components/dms/resources/img/component.png"
+      },
+      {
+        "label": "Abstract",
+        "name": "subject",
+        "type": "text",
+        "info": "A short description of your component. Ideally in title case",
+        "placeholder": "Provides Components for Creating Apps.",
+        "maxlength": 70
       },
       "<div class='well'><p>I agree that all software of my app is released as free software under the <a href='https://en.wikipedia.org/wiki/MIT_License' target='_blank'>MIT license</a>.</p><p>I agree that all content of my app will be released as public domain under the <a href='https://creativecommons.org/share-your-work/public-domain/cc0/' target='_blank'>CC0 license</a>.</p>I confirm that this does not violate the copyright of third parties.</div>",
       {
