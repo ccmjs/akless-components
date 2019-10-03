8f214ab4d82e5ca6349d7f6ababea84b5ddb4787
akless <andre.kless@web.de>
2019-10-03 23:24:54 +0200

    correct version

--- app_manager/resources/configs.js
+++ app_manager/resources/configs.js
@@ -79,7 +79,7 @@ ccm.files[ 'configs.js' ] = {
     },
     "default_icon": "https://ccmjs.github.io/akless-components/dms/resources/img/default.png",
     "form": [ "ccm.component", "https://ccmjs.github.io/akless-components/submit/versions/ccm.submit-7.1.5.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/submit/resources/configs.js", "app_meta_edit" ] ],
-    "handover_app": [ "ccm.component", "https://ccmjs.github.io/akless-components/handover_app/versions/ccm.handover_app-1.0.0.js", {
+    "handover_app": [ "ccm.component", "https://ccmjs.github.io/akless-components/handover_app/versions/ccm.handover_app-1.0.1.js", {
       "qr_code": [ "ccm.load", "https://ccmjs.github.io/akless-components/libs/qrcode-generator/qrcode.min.js" ],
       "window": [ "ccm.component", "https://ccmjs.github.io/akless-components/window/versions/ccm.window-1.0.0.js" ]
     } ],
