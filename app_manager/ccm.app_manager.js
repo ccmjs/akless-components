263bd3ee8d124c2d686555b821fdc97a14303b66
akless <andre.kless@web.de>
2019-10-03 23:24:46 +0200

    minor change

--- app_manager/ccm.app_manager.js
+++ app_manager/ccm.app_manager.js
@@ -42,7 +42,7 @@
       "data": { "store": [ "ccm.store" ] },
 //    "default_icon": "https://ccmjs.github.io/akless-components/dms/resources/img/default.png",
 //    "form": [ "ccm.component", "https://ccmjs.github.io/akless-components/submit/versions/ccm.submit-7.1.5.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/submit/resources/configs.js", "app_meta_edit" ] ],
-//    "handover_app": [ "ccm.component", "https://ccmjs.github.io/akless-components/handover_app/versions/ccm.handover_app-1.0.0.js" ],
+//    "handover_app": [ "ccm.component", "https://ccmjs.github.io/akless-components/handover_app/versions/ccm.handover_app-1.0.1.js" ],
 //    "helper": [ "ccm.load", "https://ccmjs.github.io/akless-components/modules/helper.mjs" ],
       "html": [ "ccm.load", "https://ccmjs.github.io/akless-components/app_manager/resources/templates.html" ],
 //    "lang": [ "ccm.instance", "https://ccmjs.github.io/tkless-components/lang/versions/ccm.lang-1.0.0.js" ],
