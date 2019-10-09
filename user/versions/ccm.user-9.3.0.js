c814fc4a87960b76f8ffa230e448a04030605806
akless <andre.kless@web.de>
2019-10-09 10:42:48 +0200

    ccm.user.js 9.3.0: added optional restart of parent instance after logout

--- user/versions/ccm.user-9.3.0.js
+++ user/versions/ccm.user-9.3.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for user authentication
  * @author André Kless <andre.kless@web.de> 2017-2019
  * @license The MIT License (MIT)
- * @version latest (9.3.0)
+ * @version 9.3.0
  * @changes
  * version 9.3.0 (09.10.2019):
  * - added optional restart of parent instance after logout
@@ -30,7 +30,7 @@
 
   const component = {
 
-    name: 'user',
+    name: 'user', version: [ 9, 3, 0 ],
 
     ccm: 'https://ccmjs.github.io/ccm/versions/ccm-24.0.1.js',
 
