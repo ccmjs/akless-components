2290b0fc6ea63a84cfd071c67704e0a38b4e8768
akless <andre.kless@web.de>
2019-12-22 19:41:20 +0100

    ccm.menu.js v2.9.0: added 'noclick' parameter for menu.select() to prevent click action; uses ccm v24.2.0

--- menu/versions/ccm.menu-2.9.0.js
+++ menu/versions/ccm.menu-2.9.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for rendering a menu
  * @author André Kless <andre.kless@web.de> 2015-2016, 2018-2019
  * @license The MIT License (MIT)
- * @version latest (2.9.0)
+ * @version 2.9.0
  * @changes
  * version 2.9.0 (22.12.2019):
  * - added 'noclick' parameter for menu.select() to prevent click action
@@ -62,7 +62,7 @@
 
   const component = {
 
-    name: 'menu',
+    name: 'menu', version: [ 2, 9, 0 ],
 
     ccm: 'https://ccmjs.github.io/ccm/versions/ccm-24.2.0.js',
 
