1cecfd0d682ca4544e55cb157270cdd763175fcd
akless <andre.kless@web.de>
2020-01-04 20:28:29 +0100

    added unit tests for module helper functions 'hasDomContact' and 'hasParentContact'

--- unit-tests/module-helper.js
+++ unit-tests/module-helper.js
@@ -65,64 +65,7 @@ ccm.files[ 'module-helper.js' ] = {
     }
   },
 
-/*------------------------------------------------- Data Management --------------------------------------------------*/
-
-  dataset: {
-    setup: async suite => {
-      suite.store = await suite.ccm.store();
-      suite.key = 'dataset_key';
-      suite.dataset = { key: suite.key, foo: 'bar' };
-    },
-    tests: {
-      exists: async suite => {
-        await suite.store.set( suite.dataset );
-        suite.assertEquals( suite.dataset, await suite.modules.dataset( { store: suite.store, key: suite.key } ) );
-      },
-      notExists: async suite => suite.assertEquals( { key: suite.key }, await suite.modules.dataset( { store: suite.store, key: suite.key } ) ),
-      notStored: async suite => {
-        await suite.modules.dataset( { store: suite.store, key: suite.key } );
-        suite.assertFalse( await suite.store.get( suite.key ) );
-      },
-      generated: async suite => {
-        const dataset = await suite.modules.dataset( { store: suite.store } );
-        suite.assertTrue( Object.keys( dataset ).length === 1 && ccm.helper.isKey( dataset.key ) );
-      },
-      datasetAsSettings: async suite => suite.assertEquals( suite.dataset, await suite.modules.dataset( suite.dataset ) ),
-      datasetAsKey: async suite => suite.assertEquals( suite.dataset, await suite.modules.dataset( { store: suite.store, key: suite.dataset } ) ),
-      default: async suite => suite.assertEquals( {}, await suite.modules.dataset() ),
-      cloned: async suite => suite.assertNotSame( suite.dataset, await suite.modules.dataset( suite.dataset ) ),
-      userLogin: async suite => {
-        suite.store.user = { login: () => {} };
-        await suite.store.set( suite.dataset );
-        suite.assertEquals( suite.dataset, await suite.modules.dataset( { store: suite.store, key: suite.key, login: true } ) );
-      },
-      userLoginFailed: async suite => {
-        try {
-          suite.store.user = { login: function () { throw new Error( 'Authentication failed' ); } };
-          await suite.modules.dataset( { store: suite.store, key: suite.key, login: true } );
-          suite.failed( 'Exception was not caught' );
-        }
-        catch( e ) {
-          suite.passed();
-        }
-      },
-      userSpecificKey: async suite => {
-        suite.store.user = { isLoggedIn: () => true, data: () => { return { key: 'john' } }, ccm: true, component: { Instance: true } };
-        await suite.store.set( { key: [ suite.key, 'john' ], foo: 'bar' } );
-        suite.assertEquals( { key: [ suite.key, 'john' ], foo: 'bar' }, await suite.modules.dataset( { store: suite.store, key: suite.key, user: true } ) );
-      },
-      permissions: async suite => {
-        const permissions = { creator: 'john', realm: 'guest', access: 'creator' };
-        suite.assertEquals( { key: suite.key, _: permissions }, await suite.modules.dataset( { store: suite.store, key: suite.key, permissions: permissions } ) );
-      },
-      convert: async suite => {
-        await suite.store.set( suite.dataset );
-        suite.assertEquals( { key: suite.key, foo: 'BAR' }, await suite.modules.dataset( { store: suite.store, key: suite.key, convert: dataset => { dataset.foo = dataset.foo.toUpperCase(); return dataset; } } ) );
-      }
-    }
-  },
-
-/*------------------------------------------------ Data Manipulation -------------------------------------------------*/
+/*-------------------------------------------------- Data Handling ---------------------------------------------------*/
 
   arrToObj: {
     setup: suite => suite.expected = { foo: true, bar: true },
@@ -187,6 +130,115 @@ ccm.files[ 'module-helper.js' ] = {
   unescapeHTML: {
     tests: {
       string: suite => suite.assertSame( 'Hello <b>World</b>!', suite.modules.unescapeHTML( 'Hello &lt;b&gt;World&lt;/b&gt;!' ) )
+    }
+  },
+
+/*------------------------------------------------- Data Management --------------------------------------------------*/
+
+  dataset: {
+    setup: async suite => {
+      suite.store = await suite.ccm.store();
+      suite.key = 'dataset_key';
+      suite.dataset = { key: suite.key, foo: 'bar' };
+    },
+    tests: {
+      exists: async suite => {
+        await suite.store.set( suite.dataset );
+        suite.assertEquals( suite.dataset, await suite.modules.dataset( { store: suite.store, key: suite.key } ) );
+      },
+      notExists: async suite => suite.assertEquals( { key: suite.key }, await suite.modules.dataset( { store: suite.store, key: suite.key } ) ),
+      notStored: async suite => {
+        await suite.modules.dataset( { store: suite.store, key: suite.key } );
+        suite.assertFalse( await suite.store.get( suite.key ) );
+      },
+      generated: async suite => {
+        const dataset = await suite.modules.dataset( { store: suite.store } );
+        suite.assertTrue( Object.keys( dataset ).length === 1 && ccm.helper.isKey( dataset.key ) );
+      },
+      datasetAsSettings: async suite => suite.assertEquals( suite.dataset, await suite.modules.dataset( suite.dataset ) ),
+      datasetAsKey: async suite => suite.assertEquals( suite.dataset, await suite.modules.dataset( { store: suite.store, key: suite.dataset } ) ),
+      default: async suite => suite.assertEquals( {}, await suite.modules.dataset() ),
+      cloned: async suite => suite.assertNotSame( suite.dataset, await suite.modules.dataset( suite.dataset ) ),
+      userLogin: async suite => {
+        suite.store.user = { login: () => {} };
+        await suite.store.set( suite.dataset );
+        suite.assertEquals( suite.dataset, await suite.modules.dataset( { store: suite.store, key: suite.key, login: true } ) );
+      },
+      userLoginFailed: async suite => {
+        try {
+          suite.store.user = { login: function () { throw new Error( 'Authentication failed' ); } };
+          await suite.modules.dataset( { store: suite.store, key: suite.key, login: true } );
+          suite.failed( 'Exception was not caught' );
+        }
+        catch( e ) {
+          suite.passed();
+        }
+      },
+      userSpecificKey: async suite => {
+        suite.store.user = { isLoggedIn: () => true, data: () => { return { key: 'john' } }, ccm: true, component: { Instance: true } };
+        await suite.store.set( { key: [ suite.key, 'john' ], foo: 'bar' } );
+        suite.assertEquals( { key: [ suite.key, 'john' ], foo: 'bar' }, await suite.modules.dataset( { store: suite.store, key: suite.key, user: true } ) );
+      },
+      permissions: async suite => {
+        const permissions = { creator: 'john', realm: 'guest', access: 'creator' };
+        suite.assertEquals( { key: suite.key, _: permissions }, await suite.modules.dataset( { store: suite.store, key: suite.key, permissions: permissions } ) );
+      },
+      convert: async suite => {
+        await suite.store.set( suite.dataset );
+        suite.assertEquals( { key: suite.key, foo: 'BAR' }, await suite.modules.dataset( { store: suite.store, key: suite.key, convert: dataset => { dataset.foo = dataset.foo.toUpperCase(); return dataset; } } ) );
+      }
+    }
+  },
+
+/*--------------------------------------------------- ccm Instance ---------------------------------------------------*/
+
+  hasDomContact: {
+    setup: suite => suite.element = document.createElement( 'div' ),
+    tests: {
+      contact: async suite => {
+        document.body.appendChild( suite.element );
+        const instance = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm }, { root: suite.element } );
+        suite.assertTrue( suite.modules.hasDomContact( instance ) );
+        document.body.removeChild( suite.element );
+      },
+      noContact: async suite => {
+        const instance = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm }, { root: suite.element } );
+        suite.assertFalse( suite.modules.hasDomContact( instance ) );
+      },
+      parentContact: async suite => {
+        document.body.appendChild( suite.element );
+        const parent = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm }, { root: suite.element } );
+        const instance = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm }, { parent: parent } );
+        parent.element.appendChild( instance.root );
+        suite.assertTrue( suite.modules.hasDomContact( instance ) );
+        document.body.removeChild( suite.element );
+      },
+      noParentContact: async suite => {
+        document.body.appendChild( suite.element );
+        const parent = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm }, { root: suite.element } );
+        const instance = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm }, { parent: parent } );
+        suite.assertFalse( suite.modules.hasDomContact( instance ) );
+        document.body.removeChild( suite.element );
+      }
+    }
+  },
+  hasParentContact: {
+    tests: {
+      contact: async suite => {
+        const parent = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm } );
+        const instance = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm }, { parent: parent } );
+        parent.element.appendChild( instance.root );
+        suite.assertTrue( suite.modules.hasParentContact( instance ) );
+      },
+      noContact: async suite => {
+        const parent = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm } );
+        const instance = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm }, { parent: parent } );
+        suite.assertFalse( suite.modules.hasParentContact( instance ) );
+      },
+      noParent: async suite => {
+        const instance = await suite.ccm.instance( { Instance: function () {}, ccm: suite.ccm } );
+        suite.assertFalse( suite.modules.hasParentContact( instance ) );
+      }
     }
   },
 
