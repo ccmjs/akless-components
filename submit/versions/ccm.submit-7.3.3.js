b95d90548f02ad79e6270732dd3e4a2d96ddc7e1
akless <andre.kless@web.de>
2020-02-04 08:48:00 +0100

    minor change

--- submit/versions/ccm.submit-7.3.3.js
+++ submit/versions/ccm.submit-7.3.3.js
@@ -2,7 +2,7 @@
  * @overview ccm component for submitting data
  * @author André Kless <andre.kless@web.de> 2018-2020
  * @license The MIT License (MIT)
- * @version latest (7.3.3)
+ * @version 7.3.3
  * @changes
  * version 7.3.3 (03.02.2020):
  * - bug fix when ccm-based input element returns no result
