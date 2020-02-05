2565d0873a76111eba0237eaf6195c0086a06e39
akless <andre.kless@web.de>
2020-02-05 13:10:52 +0100

    ccm.image_map_builder.js v1.0.0

---
+++ image_map_builder/resources/resources.js
@@ -0,0 +1,19 @@
+/**
+ * @overview static data-based resources of ccm component for building image maps
+ * @author André Kless <andre.kless@web.de> 2020
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ 'resources.js' ] = {
+
+  /** test configuration (relative paths) */
+  "local": {
+    "helper.1": "../modules/helper.mjs",
+    "html.1": "../image_map_builder/resources/templates.html",
+    "image_map.1": "../image_map/ccm.image_map.js",
+    "logger": [ "ccm.instance", "../log/ccm.log.js", [ "ccm.get", "../log/resources/configs.js", "greedy" ] ],
+    "onchange": instance => console.log( instance.getValue() ),
+    "submit.1": "../submit/ccm.submit.js"
+  }
+
+};
