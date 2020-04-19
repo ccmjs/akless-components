f709e993673bcdc6069f802a7201e8e5662e6ec9
akless <andre.kless@web.de>
2020-04-19 22:14:59 +0200

    ccm.testsuite.js v3.0.1 - uses ccm v25.4.0 - uses helper.mjs v5.0.0 as default - bug fix for assertSame and assertEquals

--- testsuite/versions/ccm.testsuite-3.0.1.js
+++ testsuite/versions/ccm.testsuite-3.0.1.js
@@ -2,7 +2,7 @@
  * @overview ccm component for unit tests
  * @author André Kless <andre.kless@web.de> 2016-2017, 2019-2020
  * @license The MIT License (MIT)
- * @version latest (3.0.1)
+ * @version 3.0.1
  * @changes
  * version 3.0.1 (19.04.2020)
  * - uses ccm v25.4.0
@@ -19,7 +19,7 @@
 
   const component = {
 
-    name: 'testsuite',
+    name: 'testsuite', version: [ 3, 0, 1 ],
 
     ccm: 'https://ccmjs.github.io/ccm/versions/ccm-25.4.0.js',
 
