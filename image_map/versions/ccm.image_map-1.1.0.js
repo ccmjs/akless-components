52167a73f751a8d3141d0efcb700a0345d351abf
akless <andre.kless@web.de>
2020-05-07 23:55:26 +0200

    bug fix

--- image_map/versions/ccm.image_map-1.1.0.js
+++ image_map/versions/ccm.image_map-1.1.0.js
@@ -2,7 +2,7 @@
  * @overview ccm component for an image map
  * @author André Kless <andre.kless@web.de> 2019-2020
  * @license The MIT License (MIT)
- * @version latest (1.1.0)
+ * @version 1.1.0
  * @changes
  * version 1.1.0 (02.02.2020):
  * - uses ccm v25.5.2
@@ -19,7 +19,7 @@
 
   const component = {
 
-    name: 'image_map',
+    name: 'image_map', version: [ 1, 1, 0 ],
 
     ccm: 'https://ccmjs.github.io/ccm/versions/ccm-25.5.2.js',
 
