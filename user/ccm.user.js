3455f742e0bbe88db57a156b57b34362ab00c1f1
akless <andre.kless@web.de>
2020-06-26 10:06:51 +0200

    test change

--- user/ccm.user.js
+++ user/ccm.user.js
@@ -173,13 +173,12 @@
                 result = await renderLogin( this.title, true );
                 if ( !result ) { await this.start(); throw new Error( 'login aborted' ); }
                 if ( result.user.charAt( result.user.length - 2 ) === '2' ) {
-                  /*
                   result = await this.ccm.load( {
                     url: 'https://kaul.inf.h-brs.de/cors/token.php',
                     method: 'POST',
                     params: { realm: 'hbrsinfkaul', user: result.user, password: result.token }
                   } );
-                  */
+                  /*
                   result = await this.ccm.load( {
                     url: 'https://kaul.inf.h-brs.de/cors/login.php',
                     method: 'GET',
@@ -188,6 +187,7 @@
                       'Authorization': 'Basic ' + btoa( result.user + ':' + result.token )
                     }
                   } );
+                  */
                   if ( $.isObject( result ) ) {
                     result.key = result.user;
                     result.token = result.user + '#' + result.token;
