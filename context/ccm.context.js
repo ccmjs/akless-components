5dbad4af2c79c7e6e44b46272206fbbd6ea829e4
akless <andre.kless@web.de>
2020-09-18 15:44:03 +0200

    minor change

--- context/ccm.context.js
+++ context/ccm.context.js
@@ -19,7 +19,7 @@
     Instance: function () {
       let $;
       this.init = async () => $ = Object.assign( {}, this.ccm.helper, this.helper );
-      this.start = async () => $.render( this.html.main( this.ccm.context.root( this.instance ) ), this.element );
+      this.start = async () => $.render( $.html( this.html.main, this.ccm.context.root( this.instance ) ), this.element );
     }
   };
   let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||[""])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){(c="latest"?window.ccm:window.ccm[c]).component(component);document.head.removeChild(a)};a.src=component.ccm.url}
