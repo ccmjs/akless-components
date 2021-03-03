7d53af256280eca90e17eeaa57a7f1f1e85cafbf
akless <andre.kless@web.de>
2021-03-03 13:02:14 +0100

    correct HTML template dependency

--- user/versions/ccm.user-9.7.1.js
+++ user/versions/ccm.user-9.7.1.js
@@ -25,7 +25,7 @@
 //    "guest": "guest",
 //    "hash": [ "ccm.load", "https://ccmjs.github.io/akless-components/modules/md5.mjs" ],
       "helper": [ "ccm.load", "https://ccmjs.github.io/akless-components/modules/versions/helper-7.0.0.mjs" ],
-      "html": [ "ccm.load", "https://ccmjs.github.io/akless-components/user/resources/templates.mjs" ],
+      "html": [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/resources.js", "html" ],
 //    "logged_in": true,
 //    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-5.0.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ],
 //    "map": user => user.user === 'john' ? 'Teacher' : 'Student',
