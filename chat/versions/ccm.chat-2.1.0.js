7e907dda4a10c6e58b8d5e8a2b3e0c666e390d84
akless <andre.kless@web.de>
2021-03-12 09:16:58 +0100

    minor change

--- chat/versions/ccm.chat-2.1.0.js
+++ chat/versions/ccm.chat-2.1.0.js
@@ -8,7 +8,7 @@
  * - uses ccmjs v26.2.0 as default
  * - permission settings for new chat messages can be set via config.data.permissions
  * - permission settings for new chat messages are optional (enabled by default)
- * - changed key and and added chat property for new chat messages
+ * - changed key for new chat messages
  * - refresh on realtime updates only for relevant datasets
  * version 2.0.0 (17.02.2021):
  * - uses ccmjs v26.1.1 as default
