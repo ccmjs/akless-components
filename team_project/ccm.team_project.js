cc1cb4395d3311779a261a0ae12cd96305374b1d
akless <andre.kless@web.de>
2021-04-29 17:51:35 +0200

    trigger deploy

--- team_project/ccm.team_project.js
+++ team_project/ccm.team_project.js
@@ -45,6 +45,7 @@
  */
 
 ( () => {
+
   const component = {
     name: 'team_project',
     ccm: 'https://ccmjs.github.io/ccm/versions/ccm-26.4.0.js',
@@ -219,5 +220,6 @@
 
     }
   };
+
   let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||[""])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){(c="latest"?window.ccm:window.ccm[c]).component(component);document.head.removeChild(a)};a.src=component.ccm.url}
 } )();
