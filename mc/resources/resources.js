28f653caf3d1dbc94cebe0045485dd2a97107fa9
akless <andre.kless@web.de>
2021-07-04 10:52:05 +0200

    updated test and demo configuration of ccm.mc.js: load questions from datastore

--- mc/resources/resources.js
+++ mc/resources/resources.js
@@ -17,7 +17,7 @@ ccm.files[ 'resources.js' ] = {
     "helper.1": "./../modules/helper.mjs",
     "html.1": "./../mc/resources/templates.mjs",
     "onfinish": { "log": true, "store": true, "restart": true },
-    "questions": [ "ccm.get", "./../mc/resources/resources.js", "questions" ],
+    "questions": [ "ccm.get", { "name": "dbs-questions", "url": "https://ccm2.inf.h-brs.de" } ],
     "text": {
       "question": "Frage %nr%/%total%",
       "buttons": [ "Richtig", "Enthaltung", "Falsch" ],
@@ -35,7 +35,7 @@ ccm.files[ 'resources.js' ] = {
     },
     "feedback": true,
     "onfinish": { "log": true, "store": true, "restart": true },
-    "questions": [ "ccm.get", "https://ccmjs.github.io/akless-components/mc/resources/resources.js", "questions" ],
+    "questions": [ "ccm.get", { "name": "dbs-questions", "url": "https://cm2.inf.h-brs.de" } ],
     "text": {
       "question": "Frage %nr%/%total%",
       "buttons": [ "Richtig", "Enthaltung", "Falsch" ],
@@ -43,30 +43,6 @@ ccm.files[ 'resources.js' ] = {
       "next": "Weiter",
       "submit": "Abschicken"
     }
-  },
-
-  /** example questions data */
-  "questions": [
-    {
-      "text": "<p>Gegeben sind die folgenden beiden Datensätze der Datensammlung \"Personen\":</p><ul><li>Harald Kaputnik, Hauptstraße 18, 53117 Bonn</li><li>Petra Weizenkeim, 22.06.1999, 0228-12345678, 53117 Bonn</li></ul>",
-      "answers": [
-        { "solution": true, "text": "Die Datensammlung \"Personen\" vereinigt unstrukturierte Daten." },
-        { "solution": false, "text": "Die Datensammlung \"Personen\" vereinigt semi-strukturierte Daten." },
-        { "solution": false, "text": "Die Datensammlung \"Personen\" vereinigt strukturierte Daten." },
-        { "solution": true, "text": "Die Bedeutung der Datenwerte ist für den Leser nicht ersichtlich." },
-        { "solution": false, "text": "Es könnte sich um zwei Datensätze einer Tabelle \"Personen\" handeln." }
-      ]
-    },
-    {
-      "text": "Die logische Datenunabhängigkeit bedeutet, dass ...",
-      "answers": [
-        { "solution": false, "text": "der Datenzugriff lediglich über eine Beschreibung der gewünschten Daten und nicht über die Angabe der Speicheradresse erfolgt." },
-        { "solution": false, "text": "die Datenbank eine Vereinigung der Daten vieler Anwendungsbereiche enthält." },
-        { "solution": true, "text": "die interne Darstellung der Daten geändert werden kann, ohne dass die Anwendungsprogramme angepasst werden müssen." },
-        { "solution": false, "text": "die Daten auf eine andere Datenbank verschoben werden können, ohne dass die Anwendungsprogramme angepasst werden müssen." },
-        { "solution": true, "text": "sich der Name einer Tabelle oder einer Spalte ändern kann, ohne dass die Anwendungsprogramme angepasst werden müssen." }
-      ]
-    }
-  ]
+  }
 
 };
