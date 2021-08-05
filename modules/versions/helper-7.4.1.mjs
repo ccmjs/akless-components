a44b4a76e2a78258f0db91d6c559766362c0fb01
akless <andre.kless@web.de>
2021-08-05 08:33:56 +0200

    helper.mjs v7.4.1: dataset(settings):obj returns an object in any case

--- modules/versions/helper-7.4.1.mjs
+++ modules/versions/helper-7.4.1.mjs
@@ -3,7 +3,7 @@
  * ES6 module that exports useful help functions for <i>ccmjs</i> component developers.
  * @author André Kless <andre.kless@web.de> 2019-2021
  * @license The MIT License (MIT)
- * @version latest (7.4.1)
+ * @version 7.4.1
  * @changes
  * version 7.4.1 (05.08.2021):
  * - dataset(settings):obj returns an object in any case
