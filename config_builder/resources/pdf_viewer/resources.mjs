e5be7edfd37291f477ed7bafc2131a06827527e4
akless <andre.kless@web.de>
2021-09-24 12:33:21 +0200

    added app builder for ccm.pdf_viewer.js via ccm.config_builder.js

--- config_builder/resources/pdf_viewer/resources.mjs
+++ config_builder/resources/pdf_viewer/resources.mjs
@@ -5,44 +5,42 @@
  */
 
 /**
- * texts and labels
- * @type {Object.<string,string>}
+ * basic configuration
+ * @type {Object}
  */
-const text = {
-  "denied": "Message: Access Denied",
-  "denied_info": "Message that appears in the case of a password-protected PDF when you have entered an incorrect password.",
-  "download": "Tooltip: Download PDF",
-  "download_info": "Tooltip that appears when you move the mouse cursor over the \"Download PDF\" button.",
-  "downloadable": "PDF can be downloaded",
-  "downloadable_info": "If enabled, there is an additional button in the navigation bar that can be used to download the PDF. In addition, the current page can be saved as an image by right-clicking on a page. You can prevent the PDF from being downloaded via the web interface by deactivating this field.",
-  "first": "Tooltip: First Page",
-  "first_info": "Tooltip that appears when you move the mouse cursor over the \"First Page\" button.",
-  "general": "General Settings",
-  "jump": "Tooltip: Jump to Page",
-  "jump_info": "Tooltip that appears when you move the mouse cursor over the input field that let you jump to a specific page.",
-  "labels": "Texts and Labels",
-  "last": "Tooltip: Last Page",
-  "last_info": "Tooltip that appears when you move the mouse cursor over the \"Last Page\" button.",
-  "next": "Tooltip: Next Page",
-  "next_info": "Tooltip that appears when you move the mouse cursor over the \"Next Page\" button.",
-  "pdf": "URL of the PDF",
-  "pdf_info": "If you only have the PDF as a file on your filesystem, you must first publish it under a public URL, which you then specify here. It is important that the PDF can also be accessed cross-domain using <a href='https://en.wikipedia.org/wiki/Cross-origin_resource_sharing' target='_blank'>CORS</a>. Developers familiar with <a href='https://github.com/about/' target='_blank'>GitHub</a> can use <a href='https://pages.github.com/' target='_blank'>GitHub Pages</a>. Via GitHub Pages the PDF can be published for free and CORS is already set there by default.",
-  "prev": "Tooltip: Previous Page",
-  "prev_info": "Tooltip that appears when you move the mouse cursor over the \"Previous Page\" button.",
-  "preview": "Preview",
-  "preview_title": "App Preview",
-  "protected": "Message: Protected PDF",
-  "protected_info": "Message that appears in the case of a password-protected PDF.",
-  "submit": "Submit",
-  "user": "Required Account",
-  "user_info": "Here you can set whether the PDF should only be displayed for people who log in with a specific account.<ul class=\"m-0 pl-4\"><li><b>Digital Makerspace Account:</b> The user must log in with a Digital Makerspace account.</li><li><b>H-BRS FB02 Account:</b> The user has to authenticate with an account from the Department of Computer Sciences at Hochschule Bonn-Rhein-Sieg University of Applied Sciences.</li></ul>"
+const basic = {
+  "id": "pvb",
+  "text": {
+    "downloadable": "PDF can be downloaded",
+    "downloadable_info": "If enabled, there is an additional button in the navigation bar that can be used to download the PDF. In addition, the current page can be saved as an image by right-clicking on a page. You can prevent the PDF from being downloaded via the web interface by deactivating this field.",
+    "general": "General Settings",
+    "labels": "Texts and Labels",
+    "pdf": "URL of the PDF",
+    "pdf_info": "If you only have the PDF as a file on your filesystem, you must first publish it under a public URL, which you then specify here. It is important that the PDF can also be accessed cross-domain using <a href='https://en.wikipedia.org/wiki/Cross-origin_resource_sharing' target='_blank'>CORS</a>. Developers familiar with <a href='https://github.com/about/' target='_blank'>GitHub</a> can use <a href='https://pages.github.com/' target='_blank'>GitHub Pages</a>. Via GitHub Pages the PDF can be published for free and CORS is already set there by default.",
+    "preview": "Preview",
+    "preview_title": "App Preview",
+    "submit": "Submit",
+    "text_denied": "Message: Access Denied",
+    "text_denied_info": "Message that appears in the case of a password-protected PDF when you have entered an incorrect password.",
+    "text_download": "Tooltip: Download PDF",
+    "text_download_info": "Tooltip that appears when you move the mouse cursor over the \"Download PDF\" button.",
+    "text_first": "Tooltip: First Page",
+    "text_first_info": "Tooltip that appears when you move the mouse cursor over the \"First Page\" button.",
+    "text_jump": "Tooltip: Jump to Page",
+    "text_jump_info": "Tooltip that appears when you move the mouse cursor over the input field that let you jump to a specific page.",
+    "text_last": "Tooltip: Last Page",
+    "text_last_info": "Tooltip that appears when you move the mouse cursor over the \"Last Page\" button.",
+    "text_next": "Tooltip: Next Page",
+    "text_next_info": "Tooltip that appears when you move the mouse cursor over the \"Next Page\" button.",
+    "text_prev": "Tooltip: Previous Page",
+    "text_prev_info": "Tooltip that appears when you move the mouse cursor over the \"Previous Page\" button.",
+    "text_protected": "Message: Protected PDF",
+    "text_protected_info": "Message that appears in the case of a password-protected PDF.",
+    "text_user": "Required Account",
+    "text_user_info": "Here you can set whether the PDF should only be displayed for people who log in with a specific account.<ul class=\"m-0 pl-4\"><li><b>Digital Makerspace Account:</b> The user must log in with a Digital Makerspace account.</li><li><b>H-BRS FB02 Account:</b> The user has to authenticate with an account from the Department of Computer Sciences at Hochschule Bonn-Rhein-Sieg University of Applied Sciences.</li></ul>"
+  },
+  "tool": [ "ccm.component", "https://ccmjs.github.io/tkless-components/pdf_viewer/versions/ccm.pdf_viewer-7.0.0.js" ]
 };
-
-/**
- * dependency to PDF viewer component
- * @type {string[]}
- */
-const tool = [ "ccm.component", "https://ccmjs.github.io/tkless-components/pdf_viewer/versions/ccm.pdf_viewer-7.0.0.js" ];
 
 /**
  * test configuration (relative paths)
@@ -53,11 +51,12 @@ export const local = {
     [  // serial
       "./../../../libs/bootstrap-5/css/bootstrap.css",
       "./../styles.css"
-    ]
+    ],
+    "./../../../libs/bootstrap-5/css/bootstrap-icons.css",
+    { "url": "./../../../libs/bootstrap-5/css/bootstrap-fonts.css", "context": "head" }
   ],
   "html": [ "ccm.load", "./templates.mjs" ],
-  "text": text,
-  "tool": tool
+  "src": basic
 };
 
 /**
@@ -66,6 +65,5 @@ export const local = {
  */
 export const demo = {
   "html": [ "ccm.load", "https://ccmjs.github.io/akless-components/config_builder/resources/pdf_viewer/templates.mjs" ],
-  "text": text,
-  "tool": tool
+  "src": basic
 };
--- config_builder/resources/pdf_viewer/templates.mjs
+++ config_builder/resources/pdf_viewer/templates.mjs
@@ -3,7 +3,7 @@
  * @author André Kless <andre.kless@web.de> 2021
  */
 
-import { html, render, unsafeHTML } from 'https://ccmjs.github.io/akless-components/libs/lit/lit.js';
+import { html, render, use, checkbox, text, buttons, modal } from './../templates.mjs';  // https://ccmjs.github.io/akless-components/config_builder/resources/
 export { render };
 
 /**
@@ -14,256 +14,51 @@ export { render };
  * @returns {TemplateResult} main HTML template
  */
 export function main( config, builder, events ) {
+  const id = builder.id;
+  use( config, builder, events );
   return html`
     <form @submit=${ events.onSubmit }>
-      <div class="accordion accordion-flush" id="pvb-accordion">
+      <div class="accordion accordion-flush" id="${ id }-accordion">
 
         <!-- General Settings -->
         <div class="accordion-item">
-          <h2 class="accordion-header" id="pvb-general-heading">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-general" aria-expanded="true" aria-controls="pvb-general">
+          <h2 class="accordion-header" id="${ id }-general-heading">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#${ id }-general" aria-expanded="true" aria-controls="${ id }-general">
               ${ builder.text.general }
             </button>
           </h2>
-          <div id="pvb-general" class="accordion-collapse collapse show" aria-labelledby="pvb-general-heading" data-bs-parent="#pvb-accordion">
+          <div id="${ id }-general" class="accordion-collapse collapse show" aria-labelledby="${ id }-general-heading" data-bs-parent="#${ id }-accordion">
             <div class="accordion-body">
-
-              <!-- PDF URL -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-pdf" class="form-label">
-                    ${ builder.text.pdf }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-pdf-info" aria-expanded="false" aria-controls="pvb-pdf-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-pdf-info">
-                  <div class="card card-body bg-info p-2">
-                    <small>${ unsafeHTML( builder.text.pdf_info ) }</small>
-                  </div>
-                </div>
-                <input type="url" name="pdf" class="form-control" id="pvb-pdf" .value=${ config.pdf }>
-              </div>
-
-              <!-- Downloadable -->
-              <div class="form-check form-switch heading">
-                <input class="form-check-input" type="checkbox" name="downloadable" id="pvb-downloadable" ?checked=${ config.downloadable }>
-                <label class="form-check-label ps-2" for="pvb-downloadable">${ builder.text.downloadable }</label>
-                <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-downloadable-info" aria-expanded="false" aria-controls="pvb-downloadable-info">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                  </svg>
-                </button>
-              </div>
-              <div class="collapse info" id="pvb-downloadable-info">
-                <small class="card card-body bg-info p-2">
-                  ${ builder.text.downloadable_info }
-                </small>
-              </div>
-
+              ${ text( { prop: 'pdf', type: 'url' } ) }
+              ${ checkbox( { prop: 'downloadable', switcher: true } ) }
             </div>
           </div>
         </div>
 
         <!-- Texts and Labels -->
         <div class="accordion-item">
-          <h2 class="accordion-header" id="pvb-labels-heading">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-labels" aria-expanded="false" aria-controls="pvb-labels">
+          <h2 class="accordion-header" id="${ id }-labels-heading">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${ id }-labels" aria-expanded="false" aria-controls="${ id }-labels">
               ${ builder.text.labels }
             </button>
           </h2>
-          <div id="pvb-labels" class="accordion-collapse collapse" aria-labelledby="pvb-labels-heading" data-bs-parent="#pvb-accordion">
+          <div id="${ id }-labels" class="accordion-collapse collapse" aria-labelledby="${ id }-labels-heading" data-bs-parent="#${ id }-accordion">
             <div class="accordion-body">
-
-              <!-- Tooltip: First Page -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-first" class="form-label">
-                    ${ builder.text.first }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-first-info" aria-expanded="false" aria-controls="pvb-first-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-first-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.first_info }
-                  </small>
-                </div>
-                <input type="text" name="text.first" class="form-control" id="pvb-first" .value=${ config.text.first }>
-              </div>
-
-              <!-- Tooltip: Previous Page -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-prev" class="form-label">
-                    ${ builder.text.prev }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-prev-info" aria-expanded="false" aria-controls="pvb-prev-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-prev-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.prev_info }
-                  </small>
-                </div>
-                <input type="text" name="text.prev" class="form-control" id="pvb-prev" .value=${ config.text.prev }>
-              </div>
-
-              <!-- Tooltip: Next Page -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-next" class="form-label">
-                    ${ builder.text.next }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-next-info" aria-expanded="false" aria-controls="pvb-next-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-next-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.next_info }
-                  </small>
-                </div>
-                <input type="text" name="text.next" class="form-control" id="pvb-next" .value=${ config.text.next }>
-              </div>
-
-              <!-- Tooltip: Last Page -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-last" class="form-label">
-                    ${ builder.text.last }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-last-info" aria-expanded="false" aria-controls="pvb-last-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-last-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.last_info }
-                  </small>
-                </div>
-                <input type="text" name="text.last" class="form-control" id="pvb-last" .value=${ config.text.last }>
-              </div>
-
-              <!-- Tooltip: Jump to Page -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-jump" class="form-label">
-                    ${ builder.text.jump }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-jump-info" aria-expanded="false" aria-controls="pvb-jump-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-jump-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.jump_info }
-                  </small>
-                </div>
-                <input type="text" name="text.jump" class="form-control" id="pvb-jump" .value=${ config.text.jump }>
-              </div>
-
-              <!-- Tooltip: Download PDF -->
-              <div class="mb-3" ?data-hidden=${ !config.downloadable }>
-                <div class="heading">
-                  <label for="pvb-download" class="form-label">
-                    ${ builder.text.download }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-download-info" aria-expanded="false" aria-controls="pvb-download-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-download-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.download_info }
-                  </small>
-                </div>
-                <input type="text" name="text.download" class="form-control" id="pvb-download" .value=${ config.text.download }>
-              </div>
-
-              <!-- Message: Protected PDF -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-protected" class="form-label">
-                    ${ builder.text.protected }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-protected-info" aria-expanded="false" aria-controls="pvb-protected-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-protected-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.protected_info }
-                  </small>
-                </div>
-                <input type="text" name="text.protected" class="form-control" id="pvb-protected" .value=${ config.text.protected }>
-              </div>
-
-              <!-- Message: Access Denied -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-denied" class="form-label">
-                    ${ builder.text.denied }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-denied-info" aria-expanded="false" aria-controls="pvb-denied-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-denied-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.denied }
-                  </small>
-                </div>
-                <input type="text" name="text.denied" class="form-control" id="pvb-denied" .value=${ config.text.denied }>
-              </div>
-
+              ${ text( 'text.first' ) }
+              ${ text( 'text.prev' ) }
+              ${ text( 'text.next' ) }
+              ${ text( 'text.last' ) }
+              ${ text( 'text.jump' ) }
+              ${ text( { prop: 'text.download', hidden: !config.downloadable } ) }
+              ${ text( 'text.protected' ) }
+              ${ text( 'text.denied' ) }
             </div>
           </div>
         </div>
       </div>
-      <div class="d-grid">
-        <button type="button" class="btn btn-info rounded-0" data-bs-toggle="modal" data-bs-target="#pvb-preview" ?data-hidden=${ !builder.text.preview } @click=${ () => events.onPreview() }>
-          ${ builder.text.preview }
-        </button>
-        <button class="btn btn-primary rounded-0" type="submit" ?data-hidden=${ !builder.onfinish || !builder.text.submit }>
-          ${ builder.text.submit }
-        </button>
-      </div>
+      ${ buttons() }
     </form>
-
-    <!-- Modal: Preview -->
-    <div class="modal fade" id="pvb-preview" tabindex="-1" aria-labelledby="pvb-preview-title" aria-hidden="true">
-      <div class="modal-dialog modal-fullscreen">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="pvb-preview-title">${ builder.text.preview_title }</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body"></div>
-        </div>
-      </div>
-    </div>
+    ${ modal() }
   `;
 
 }
