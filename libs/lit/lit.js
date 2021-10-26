200c46d582e2437104e9d914e1d19b7a4d065a2c
akless <andre.kless@web.de>
2021-10-26 16:59:38 +0200

    added ifDefined directive in lit-html

--- libs/lit/lit.js
+++ libs/lit/lit.js
@@ -1660,4 +1660,26 @@ const unsafeHTML = directive((value) => (part) => {
     previousValues.set(part, { value, fragment });
 });
 
-export { html, render, repeat, unsafeHTML };
+/**
+ * For AttributeParts, sets the attribute if the value is defined and removes
+ * the attribute if the value is undefined.
+ *
+ * For other part types, this directive is a no-op.
+ */
+const ifDefined = directive((value) => (part) => {
+    const previousValue = previousValues.get(part);
+    if (value === undefined && part instanceof AttributePart) {
+        // If the value is undefined, remove the attribute, but only if the value
+        // was previously defined.
+        if (previousValue !== undefined || !previousValues.has(part)) {
+            const name = part.committer.name;
+            part.committer.element.removeAttribute(name);
+        }
+    }
+    else if (value !== previousValue) {
+        part.setValue(value);
+    }
+    previousValues.set(part, value);
+});
+
+export { html, render, repeat, unsafeHTML, ifDefined };
