27b2145593c5adbe4831505856fc362a22de903b
akless <andre.kless@web.de>
2021-11-20 07:58:20 +0100

    bugfix

--- selectize/versions/ccm.selectize-1.0.0.js
+++ selectize/versions/ccm.selectize-1.0.0.js
@@ -51,7 +51,7 @@
           onChange: this.onchange
         } );
       };
-      this.getValue = () => $.clone( selectize.items );
+      this.getValue = () => [ ...selectize.items ];
     }
   };
   let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||[""])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){(c="latest"?window.ccm:window.ccm[c]).component(component);document.head.removeChild(a)};a.src=component.ccm.url}
