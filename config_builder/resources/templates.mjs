f331e0416848b1b14ea9f850365040e027cfa8c2
akless <andre.kless@web.de>
2022-01-25 08:02:29 +0100

    update

--- config_builder/resources/templates.mjs
+++ config_builder/resources/templates.mjs
@@ -102,7 +102,7 @@ export function checkbox( setup ) {
       </button>
     </div>
     <div class="collapse info" id="${ id }-${ key }-info">
-      <small class="card card-body bg-info p-2">
+      <small class="card card-body d-inline-block bg-info p-2">
         <span data-lang="${ key }_info">${ unsafeHTML( builder.text[ key + '_info' ] ) }</span>
       </small>
     </div>
