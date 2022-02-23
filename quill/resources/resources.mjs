f562aa9768b0ac71fcf9be57cdfe59cd9686a28c
akless <andre.kless@web.de>
2022-02-23 17:36:05 +0100

    update

--- quill/resources/resources.mjs
+++ quill/resources/resources.mjs
@@ -26,7 +26,6 @@ export const test = {
     "./../libs/quill-1/quill.snow.css",
     "./../quill/resources/styles.css"
   ] ],
-//"data": { "value": "<ccm-app component=\"https://ccmjs.github.io/eild/mc/ccm.mc.min.js\" src=\"[&quot;ccm.get&quot;,{&quot;name&quot;:&quot;dms2-configs&quot;,&quot;url&quot;:&quot;https://ccm2.inf.h-brs.de&quot;},[&quot;mc&quot;,&quot;1643700693147X19795855069461665&quot;]]\"></ccm-app><p><br></p>" },
   "embed": {
     "icon": "<img src='./../dms/resources/icon-18.png' title='Über dieses Icon kann eine App aus dem Digital Makerspace eingefügt werden.'>",
     "prompt": "Einbettungscode oder App-URL:"
@@ -41,4 +40,10 @@ export const test = {
  * demo configuration (absolute paths)
  * @type {Object}
  */
-export const demo = {};
+export const demo = {
+  "embed": {
+    "icon": "<img src='https://ccmjs.github.io/akless-components/dms/resources/icon-18.png' title='Über dieses Icon kann eine App aus dem Digital Makerspace eingefügt werden.'>",
+    "prompt": "Einbettungscode oder App-URL:"
+  },
+  "html": true,
+};
