de39a2ce2aa7670a5a05e3dfcba2d7b3f7d6c1ef
akless <andre.kless@web.de>
2022-03-01 16:05:11 +0100

    update

--- modules/configs.mjs
+++ modules/configs.mjs
@@ -245,3 +245,345 @@ export const submit_app_collection = {
     }
   }
 };
+
+export const submit_quiz = {
+  "entries": [
+    {
+      "name": "css",
+      "type": "hidden"
+    },
+    {
+      "name": "data",
+      "type": "hidden"
+    },
+    {
+      "name": "data.key",
+      "type": "key"
+    },
+    {
+      "name": "onfinish",
+      "type": "hidden"
+    },
+    "<legend class='text-primary'>Fragen und Antworten</legend>",
+    {
+      "name": "questions",
+      "type": "several",
+      "items": [
+        {
+          "label": "Frage",
+          "name": "text",
+          "type": "text",
+          "info": "Hier den Text der Frage eingeben. Weitere Fragen können über den entsprechenden Plus- und Minus-Button hinzugefügt und entfernen werden."
+        },
+        {
+          "label": "Typ",
+          "name": "input",
+          "type": "radio",
+          "info": "Wähle Single Choice, wenn nur eine Antwort oder Multiple Choice, wenn mehrere Antworten ausgewählt werden können.",
+          "items": [
+            {
+              "label": "Single Choice",
+              "value": "radio"
+            },
+            {
+              "label": "Multiple Choice",
+              "value": "checkbox"
+            }
+          ]
+        },
+        {
+          "label": "Beschreibung",
+          "name": "description",
+          "type": "textarea",
+          "info": "Hier kann eine zusätzliche Beschreibung zur Frage angegeben werden."
+        },
+        "<br>",
+        {
+          "name": "answers",
+          "type": "several",
+          "items": [
+            {
+              "label": "Antwort",
+              "name": "text",
+              "type": "text",
+              "info": "Hier den Text für eine Antwortmöglichksit eingeben. Weitere Antworten können über den entsprechenden Plus- und Minus-Button hinzugefügt und entfernen werden."
+            },
+            {
+              "label": "Correct",
+              "name": "correct",
+              "type": "checkbox",
+              "info": "Gibt an, ob diese Antwort korrekt ist."
+            },
+            {
+              "label": "Comment",
+              "name": "comment",
+              "type": "text",
+              "info": "in Kommentar kann einen Hinweis darauf geben, warum eine Antwort richtig oder falsch ist. Der Kommentar wird dann zusammen mit dem direkten Feedback angezeigt."
+            }
+          ]
+        }
+      ]
+    },
+    "<br>",
+    "<legend class=\"text-primary\">Erweiterte Einstellungen</legend>",
+    {
+      "label": "START-Button",
+      "name": "start_button",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, wird ein START-BUTTON angezeigt, bevor das Quiz beginnt."
+    },
+    {
+      "label": "Feedback",
+      "name": "feedback",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, wird nach der Beantwortung einer Frage ein direktes Feedback angezeigt."
+    },
+    {
+      "label": "Fortschrittsbalken",
+      "name": "progress_bar",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, wird ein Fortschrittsbalken angezeigt, wenn das Quiz abgeschlossen ist."
+    },
+    {
+      "label": "Navigation",
+      "name": "navigation",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, kann zwischen den Fragen navigiert werden."
+    },
+    {
+      "label": "Überspringbar",
+      "name": "skippable",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, können Fragen übersprungen werden."
+    },
+    {
+      "label": "Abschluss jederzeit möglich",
+      "name": "anytime_finish",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, müssen nicht alle Fragen beantwortet werden, um das Quiz abzuschließen."
+    },
+    {
+      "label": "Zeitlimit",
+      "name": "time",
+      "type": "number",
+      "min": 1,
+      "info": "Falls angegeben, hat das Quiz ein Zeitlimit und wird automatisch nach der angegebenen Anzahl von Sekunden abgeschlossen."
+    },
+    {
+      "label": "Fragen mischen",
+      "name": "shuffle",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, werden die Fragen gemischt, sodass ihre Reihenfolge bei jedem Start zufällig ist."
+    },
+    {
+      "label": "Antworten mischen",
+      "name": "random",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, werden die Antworten auf die Fragen in zufälliger Reihenfolge angezeigt."
+    },
+    {
+      "label": "HTML-Code nicht auswerten",
+      "name": "escape",
+      "type": "checkbox",
+      "info": "Wenn aktiviert, wird in Fragen und Antworten enthaltener HTML-Code nicht ausgewertet, sondern unverändert angezeigt."
+    },
+    {
+      "label": "Ergebnisse speichern",
+      "name": "data.store",
+      "type": "select",
+      "info": "Wähle aus, wo die Ergebnisse gespeichert werden sollen.<ul class=\"m-0 pl-4\"><b>Lokale Datenbank:</b> Die Ergebnisse werden in der lokalen Datenbank des Webbrowsers gespeichert und sind somit nur auf diesem Gerät und auch offline verfügbar.</li><li><b>Serverseitige Datenbank:</b> Die Ergebnisse werden in einer serverseitigen Datenbank gespeichert. Konkret handelt es sich um einen Datenserver des Fachbereichs Informatik der Hochschule Bonn-Rhein-Sieg in Deutschland.</li></ul>",
+      "items": [
+        {
+          "inner": "Deaktiviert",
+          "value": "[%'%ccm.store%'%]"
+        },
+        {
+          "inner": "Lokale Datenbank",
+          "value": "[%'%ccm.store%'%,{%'%name%'%:%'%quiz_results%'%}]"
+        },
+        {
+          "inner": "Serverseitige Datenbank",
+          "value": "[%'%ccm.store%'%,{%'%name%'%:%'%quiz_results%'%,%'%url%'%:%'%https://ccm2.inf.h-brs.de%'%}]"
+        }
+      ]
+    },
+    {
+      "label": "Benutzeranmeldung",
+      "name": "user",
+      "type": "select",
+      "info": "Wählen aus, wie sich ein Benutzer beim Abschließen des Quiz anmelden muss, damit sein Ergebnis gespeichert werden kann.<ul class=\"m-0 pl-4\"><li><b>Gastmodus:</b> Der Benutzer kann sich mit einem beliebigen Benutzernamen und ohne Passwort anmelden.</li><li><b>Digital Makerspace Account:</b> Der Benutzer muss sich mit einem Digital Makerspace-Account anmelden.</li><li><b>H-BRS FB02 Account:</b> Der Nutzer muss sich mit einem Account des Fachbereichs Informatik der Hochschule Bonn-Rhein-Sieg authentifizieren.</li><li><b>H-BRS FB02 Account mit Pseudonym:</b> Dasselbe wie die vorherige Option, jedoch wird der Benutzername durch ein Pseudonym ersetzt.</li><li><b>Einmaliges Pseudonym:</b> Der Benutzer wird automatisch mit einem einmaligen Pseudonym angemeldet. Jede Anmeldung gibt nach dem Ablauf einer Session ein anderes Pseudonym zurück.</li></ul>",
+      "items": [
+        {
+          "inner": "Deaktiviert",
+          "value": "null"
+        },
+        {
+          "inner": "Gastmodus",
+          "value": "[%'%ccm.instance%'%,%'%https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.3.1.js%'%]"
+        },
+        {
+          "inner": "DMS Account",
+          "value": "[%'%ccm.instance%'%,%'%https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.3.1.js%'%,{%'%hash%'%:[%'%ccm.load%'%,{%'%url%'%:%'%https://ccmjs.github.io/akless-components/modules/md5.mjs%'%,%'%type%'%:%'%module%'%}],%'%realm%'%:%'%cloud%'%,%'%store%'%:%'%dms-user%'%,%'%url%'%:%'%https://ccm2.inf.h-brs.de%'%}]"
+        },
+        {
+          "inner": "H-BRS FB02 Account",
+          "value": "[%'%ccm.instance%'%,%'%https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.3.1.js%'%,{%'%key%'%:%'%hbrsinfkaul%'%,%'%realm%'%:%'%hbrsinfkaul%'%}]"
+        },
+        {
+          "inner": "H-BRS FB02 Account mit Pseudonym",
+          "value": "[%'%ccm.instance%'%,%'%https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.3.1.js%'%,{%'%key%'%:%'%hbrsinfpseudo%'%,%'%realm%'%:%'%hbrsinfpseudo%'%}]"
+        },
+        {
+          "inner": "Einmaliges Pseudonym",
+          "value": "[%'%ccm.instance%'%,%'%https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.3.1.js%'%,{%'%key%'%:%'%pseudo%'%,%'%realm%'%:%'%guest%'%,%'%guest%'%:true}]"
+        }
+      ]
+    },
+    "<br>",
+    "<legend class=\"text-primary\">Abschluss des Quiz</legend>",
+    {
+      "label": "Bestätigungs-Dialog",
+      "name": "onfinish.confirm",
+      "type": "text",
+      "info": "Wenn aktiv, muss der Benutzer vor dem Speichern einer Lösung dies explizit bestätigen. Um dies zu aktivieren, geben Sie den Text an, der dem Benutzer im Bestätigung-Dialog angezeigt wird."
+    },
+    {
+      "label": "Leerer Inhalt",
+      "name": "onfinish.clear",
+      "type": "checkbox",
+      "info": "Wenn aktiv, wird der Quiz nach Abschluss ausgeblendet."
+    },
+    {
+      "label": "Neustarten",
+      "name": "onfinish.restart",
+      "type": "checkbox",
+      "info": "Gibt an, ob der Quiz nach Abschluss neu gestartet wird."
+    },
+    {
+      "label": "Erfolgs-Dialog",
+      "name": "onfinish.alert",
+      "type": "text",
+      "info": "Meldung, die dem Benutzer angezeigt wird, wenn die eingereichte Lösung erfolgreich gespeichert wurde."
+    },
+    "<br>",
+    "<legend class=\"text-primary\">Beschriftung von Buttons</legend>",
+    {
+      "label": "Vorherige Frage",
+      "name": "placeholder.prev",
+      "type": "text",
+      "info": "Beschriftung für den Button, mit dem man zur vorherigen Frage wechselt."
+    },
+    {
+      "label": "Abschicken",
+      "name": "placeholder.submit",
+      "type": "text",
+      "info": "Beschriftung für den Button, mit dem man die Lösung zu einer Frage abschickt."
+    },
+    {
+      "label": "Nächste Frage",
+      "name": "placeholder.next",
+      "type": "text",
+      "info": "Beschriftung für den Button, mit dem man zur nächstemn Frage wechselt."
+    },
+    {
+      "label": "Abschließen",
+      "name": "placeholder.finish",
+      "type": "text",
+      "info": "Beschriftung für den Button, mit dem man den Quiz abschließt."
+    }
+  ],
+  "ignore": {
+    "defaults": {
+      "css": [
+        "ccm.load",
+        "https://ccmjs.github.io/akless-components/quiz/resources/default.css"
+      ],
+      "helper": [
+        "ccm.load",
+        "https://ccmjs.github.io/akless-components/modules/versions/helper-4.1.0.mjs"
+      ],
+      "html": [
+        "ccm.load",
+        "https://ccmjs.github.io/akless-components/quiz/resources/templates.html"
+      ],
+      "placeholder": {
+        "cancel": "Abbrechen",
+        "prev": "Vorherige Frage",
+        "submit": "Abschicken",
+        "next": "Nächste Frage",
+        "correct": "Korrekte Lösung: ",
+        "finish": "Quiz abschließen"
+      },
+      "feedback": true,
+      "progress_bar": true,
+      "questions": [
+        {
+          "text": "Erste Frage",
+          "input": "radio",
+          "answers": [
+            {
+              "text": "Antwort A",
+              "correct": true
+            },
+            {
+              "text": "Antwort B"
+            }
+          ]
+        }
+      ]
+    }
+  },
+  "_": {
+    "access": {
+      "get": "all",
+      "set": "creator",
+      "del": "creator"
+    },
+    "creator": "akless",
+    "realm": "cloud"
+  },
+  "meta": [
+    {
+      "name": "dms-apps",
+      "url": "https://ccm2.inf.h-brs.de"
+    },
+    "1581669159921X781491117126752"
+  ],
+  "updated_at": "2020-11-04T10:25:36+01:00",
+  "created_at": "2020-02-14T09:32:41+01:00",
+  "css": [
+    "ccm.load",
+    [
+      [
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
+        {
+          "url": "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
+          "context": "head"
+        },
+        {
+          "url": "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp",
+          "type": "css"
+        },
+        {
+          "url": "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp",
+          "type": "css",
+          "context": "head"
+        }
+      ],
+      "https://ccmjs.github.io/akless-components/submit/resources/default_b4.css"
+    ]
+  ],
+  "data": {
+    "store": [
+      "ccm.store"
+    ]
+  },
+  "helper": [
+    "ccm.load",
+    "https://ccmjs.github.io/akless-components/modules/versions/helper-4.0.2.mjs"
+  ],
+  "html": [
+    "ccm.load",
+    "https://ccmjs.github.io/akless-components/submit/resources/templates_b4.html"
+  ]
+};
