33b4ef6023f6d0b2f7bb1dc04886b1128dfddf7a
akless <andre.kless@web.de>
2022-03-30 14:29:56 +0200

    helper.mjs v8.1.1: bugfix for fillForm(elem,obj):void - set no value on undefined instead of falsy

--- modules/versions/helper-8.1.1.mjs
+++ modules/versions/helper-8.1.1.mjs
@@ -3,7 +3,7 @@
  * ES6 module that exports useful help functions for <i>ccmjs</i> component developers.
  * @author André Kless <andre.kless@web.de> 2019-2022
  * @license The MIT License (MIT)
- * @version latest (8.1.1)
+ * @version 8.1.1
  * @changes
  * version 8.1.1 (30.03.2022):
  * - bugfix for fillForm(elem,obj):void - set no value on undefined instead of falsy
